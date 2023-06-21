bab9a0a8967a680487998849c7186e26113cca1f
KimKW1007 <face676934@gmail.com>
2023-06-21 23:21:43 +0900

    feat : flex css

---
+++ front/src/components/common/CommonFlex.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AllCenterFlex = styled.div`
+  display: flex;
+  align-items:center;
+  justify-content: center;
+`
+export const OnlyAlignCenterFlex = styled.div`
+  display: flex;
+  align-items:center;
+`
+export const OnlyJustifyCenterFlex = styled.div`
+  display: flex;
+  justify-content: center;
+`
