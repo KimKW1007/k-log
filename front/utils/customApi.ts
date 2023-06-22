794203466e8a55ceb50dedd38e55c202d9e12d65
KimKW1007 <face676934@gmail.com>
2023-06-22 20:59:07 +0900

    feat : customapi

---
+++ front/utils/customApi.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const axiosBase = axios.create({
+  headers:{
+    "Content-Type": "application/json",
+  }
+})
+
+const baseApi = () => {
+  axiosBase.interceptors.response.use(
+    response => response,
+    error => {
+      throw error;
+    },
+  );
+
+  return axiosBase;
+};
+
+export default function customApi<T = any>(url: string) {
+  const postApi = async (data: T) => {
+    const result = await baseApi().post(url, data);
+    return result.data;
+  };
+  const getApi = async () => {
+    const result = await baseApi().get(url);
+    return result.data;
+  };
+
+  const deleteApi = async (data: T) => {
+    const result = await baseApi().delete(url, { data });
+    return result.data;
+  };
+  const putApi = async (data: T) => {
+    const result = await baseApi().put(url, data);
+    return result.data;
+  };
+  return { postApi, getApi, deleteApi, putApi };
+}
