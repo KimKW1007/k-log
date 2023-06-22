381b7d95f8d52a333b763f754db153130f1231b3
KimKW1007 <face676934@gmail.com>
2023-06-22 20:18:48 +0900

    feat : tanstack-query

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -3,15 +3,23 @@ import type { AppProps } from 'next/app';
 import { theme } from '@styles/theme';
 import { GlobalStyle } from '@styles/global-style';
 import Layout from '@components/layout/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+const queryClient = new QueryClient({});
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
