43f180f323f14175caefbb88c6bedbdb4a7ac82a
KimKW1007 <face676934@gmail.com>
2023-06-24 09:12:10 +0900

    test : recoil persist userInfomation

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,8 +1,12 @@
 import { Header } from '@components/header/Header'
 import type { NextPage } from 'next'
+import { userInfomation } from '@src/atoms/atoms';
+import { useRecoilState } from 'recoil';
 
 
 const Home: NextPage = () => {
+  const [user, setUser] = useRecoilState(userInfomation);
+  console.log(user)
   return (
     <div>
       <Header/>
