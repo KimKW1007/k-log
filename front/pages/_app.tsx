35ae88e7046e6fa7d7028257b30e224c19b66c8f
KimKW1007 <face676934@gmail.com>
2023-06-24 09:10:21 +0900

    chore: recoilRoot 추가

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -5,22 +5,24 @@ import { GlobalStyle } from '@styles/global-style';
 import Layout from '@components/layout/Layout';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
+import { RecoilRoot } from 'recoil';
 
 const queryClient = new QueryClient({});
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </RecoilRoot>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
-}
+};
 
 export default MyApp;
