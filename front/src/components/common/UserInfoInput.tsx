80adf41c9dc37b94aa6669d600f2184c45f67fab
KimKW1007 <face676934@gmail.com>
2023-06-26 16:23:23 +0900

    style : small 값 추가

--- front/src/components/common/UserInfoInput.tsx
+++ front/src/components/common/UserInfoInput.tsx
@@ -8,14 +8,15 @@ interface InputProps {
   register: UseFormRegisterReturn;
   watch ?: UseFormWatch<FieldValues> | string;
   bold ?: boolean;
+  small ?: boolean;
 }
 
-const UserInfoInput = ({ type, inputName, watch, register, bold }: InputProps) => {
+const UserInfoInput = ({ type, inputName, watch, register, bold, small }: InputProps) => {
   const [isFocus, setIsFocus] = useState(false);
   return (
     <InputBox>
-      <CurrentInputName className={isFocus ? "high" : ""}>{inputName}</CurrentInputName>
-      <Input bold={bold ?? false} type={type} {...register}  onFocus={()=>{setIsFocus(true)}} onBlur={()=>{!watch && setIsFocus(false)}} />
+      <CurrentInputName className={isFocus ? "high" : ""} small={small ?? false}>{inputName}</CurrentInputName>
+      <Input small={small ?? false} bold={bold ?? false} type={type} {...register}  onFocus={()=>{setIsFocus(true)}} onBlur={()=>{!watch && setIsFocus(false)}} />
     </InputBox>
   );
 };
@@ -35,7 +36,7 @@ const InputBox = styled.div`
   }
 `;
 
-const Input = styled.input<{bold : boolean}>`
+const Input = styled.input<{bold : boolean, small: boolean}>`
   position:relative;
   z-index:2;
   width: 100%;
@@ -47,9 +48,15 @@ const Input = styled.input<{bold : boolean}>`
   ${({bold}) => bold && css`
     font-weight:bold;
   `}
+  ${({small, theme}) => small && css`
+    line-height: ${theme.rem.p28};
+    padding : ${theme.rem.p22} ${theme.rem.p12} ${theme.rem.p10};
+    font-size: ${theme.rem.p16};
+  `}
+
 `;
 
-const CurrentInputName = styled.span`
+const CurrentInputName = styled.span<{ small: boolean}>`
   position:absolute;
   z-index:1;
   top: 50%;
@@ -58,11 +65,17 @@ const CurrentInputName = styled.span`
   letter-spacing:1px;
   transition: .3s;
   font-weight: bold;
+  ${({small, theme}) => small && css`
+  font-size : 14px;
+`}
   &.high{
     z-index:3;
     top: 25%;
     left: 3%;
     font-size : 13px;
     pointer-events: none;
+    ${({small, theme}) => small && css`
+      font-size : 12px;
+    `}
   }
 `;
