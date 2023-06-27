8b5489fe7a519306b0a9960513487a54e2863802
KimKW1007 <face676934@gmail.com>
2023-06-27 18:53:31 +0900

    feat : checkBoxInputs

--- front/src/components/common/CheckBoxInputs.tsx
+++ front/src/components/common/CheckBoxInputs.tsx
@@ -1,58 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { OnlyAlignCenterFlex } from './CommonFlex';
 import { ChildrenProps } from '@components/layout/Layout';
-import checkedIcon from "@images/check.svg"
-interface CheckBoxProps extends ChildrenProps{
-  id : string;
-  onClick : any;
+import checkedIcon from '@images/check.svg';
+import { ArrowUpRightSquareFill } from '@styled-icons/bootstrap';
+import CommonModal from '@components/modal/CommonModal';
+interface CheckBoxProps extends ChildrenProps {
+  id: string;
+  onClick: any;
 }
 
-const CheckBoxInputs = ({children, id, onClick} : CheckBoxProps) => {
+const CheckBoxInputs = ({ children, id, onClick }: CheckBoxProps) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
     <InputBox>
-      <CheckBoxInput className='blind' type='checkbox' id={id} onClick={onClick}/>
-      <Label htmlFor={id}>
-        {children}
-      </Label>
+      <CheckBoxInput className="blind" type="checkbox" id={id} onClick={onClick} />
+      <Label htmlFor={id}>{children}</Label>
+      <DetailBtn
+        type="button"
+        onClick={() => {
+          setIsOpenModal(true);
+        }}>
+        <ArrowUpRightSquareFill />
+      </DetailBtn>
+      {isOpenModal && <CommonModal setIsOpenModal={setIsOpenModal}>아직 준비 중 입니다.</CommonModal>}
     </InputBox>
-  )
-}
+  );
+};
 
-export default CheckBoxInputs
+export default CheckBoxInputs;
 
-const InputBox = styled.div`
-  width:100%;
-
-`
+const InputBox = styled(OnlyAlignCenterFlex)`
+  width: 100%;
+  & + & {
+    margin-top: 30px;
+  }
+  justify-content: space-between;
+`;
 
 const CheckBoxInput = styled.input`
-
-  &:checked + label{
-    &::before{
-      background : url(${checkedIcon.src}) center center no-repeat;
-      background-color : #000;
-      
+  &:checked + label {
+    &::before {
+      background: url(${checkedIcon.src}) center center no-repeat;
+      background-color: #000;
     }
   }
-
-`
+`;
 
 const Label = styled.label`
   position: relative;
   display: inline-flex;
-  font-size: ${({theme})=> theme.rem.p20};
-  &:before{
+  font-size: ${({ theme }) => theme.rem.p24};
+  &:before {
     position: relative;
     z-index: 1;
-    content:"";
-    width:1em;
-    height:1em;
-    border:1px solid #000;
-    margin :auto 0;
-    transition:background-color .3s ;
+    content: '';
+    width: 0.8em;
+    height: 0.8em;
+    border: 1px solid #000;
+    margin: auto 0;
+    transition: background-color 0.3s;
     border-radius: 4px;
     margin-right: 20px;
   }
-`
+`;
 
+const DetailBtn = styled.button`
+  width: ${({ theme }) => theme.rem.p24};
+  height: ${({ theme }) => theme.rem.p24};
+`;
