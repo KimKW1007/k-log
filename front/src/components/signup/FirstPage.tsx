ce815e089440bacd32de54f68b2acb2acd942185
KimKW1007 <face676934@gmail.com>
2023-06-27 18:55:39 +0900

    chore : innerText 변경 및 style 수정

--- front/src/components/signup/FirstPage.tsx
+++ front/src/components/signup/FirstPage.tsx
@@ -1,31 +1,48 @@
-import CheckBoxInputs from '@components/common/CheckBoxInputs'
-import React, { useEffect, useState } from 'react'
+import CheckBoxInputs from '@components/common/CheckBoxInputs';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 interface isAllCheckedProps {
-  setIsAllChecked : React.Dispatch<React.SetStateAction<boolean>>
+  setIsAllChecked: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const FirstPage = ({setIsAllChecked} : isAllCheckedProps ) => {
+const FirstPage = ({ setIsAllChecked }: isAllCheckedProps) => {
   const [isAgree, setIsAgree] = useState([false, false]);
-  const onClickAgree = (idx: number) => ()=>{
+  const onClickAgree = (idx: number) => () => {
     const newIsAgree = [...isAgree];
-    newIsAgree[idx] = !newIsAgree[idx]
+    newIsAgree[idx] = !newIsAgree[idx];
     setIsAgree(newIsAgree);
-  }
-  useEffect(()=>{
-    if(isAgree[0] && isAgree[1]){
-      setIsAllChecked(true)
-    }else{
-      setIsAllChecked(false)
+  };
+  useEffect(() => {
+    if (isAgree[0] && isAgree[1]) {
+      setIsAllChecked(true);
+    } else {
+      setIsAllChecked(false);
     }
-    return ()=>{}
-  },[isAgree])
+    return () => {};
+  }, [isAgree]);
   return (
-    <div>
-      <CheckBoxInputs id="id" onClick={onClickAgree(0)}>계정동의</CheckBoxInputs>
-      <CheckBoxInputs id="id1" onClick={onClickAgree(1)}>계정동의</CheckBoxInputs>
-    </div>
-  )
-}
+    <React.Fragment>
+      <CheckInputsBox>
+        <CheckBoxInputs id="id" onClick={onClickAgree(0)}>
+          K-log 서비스 약관에 동의 &#40;필수&#41;
+        </CheckBoxInputs>
+        <CheckBoxInputs id="id1" onClick={onClickAgree(1)}>
+          개인정보 수집 및 이용에 동의 &#40;필수&#41;
+        </CheckBoxInputs>
+      </CheckInputsBox>
+      <EmptyBox/>
+    </React.Fragment>
+  );
+};
 
-export default FirstPage
+export default FirstPage;
+
+export const EmptyBox = styled.div`
+  width: 100%;
+  flex-grow: 1;
+`;
+
+const CheckInputsBox = styled.div`
+  width: 100%;
+`
