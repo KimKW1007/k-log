c280540fb7d4c6ff0f34f77562bdab4a7e8160fd
KimKW1007 <face676934@gmail.com>
2023-06-27 18:54:11 +0900

    style & refactor : error 관련 css 추가 및 구조 변경

--- front/src/components/common/UserInfoInput.tsx
+++ front/src/components/common/UserInfoInput.tsx
@@ -1,81 +1,154 @@
 import React, { useState } from 'react';
 import { FieldValues, UseFormRegisterReturn, UseFormWatch } from 'react-hook-form';
 import styled, { css } from 'styled-components';
-
+import { ExclamationDiamondFill, ExclamationCircleFill} from "@styled-icons/bootstrap"
+import { OnlyAlignCenterFlex } from './CommonFlex';
 interface InputProps {
   type: string;
   inputName: string;
   register: UseFormRegisterReturn;
-  watch ?: UseFormWatch<FieldValues> | string;
-  bold ?: boolean;
-  small ?: boolean;
+  watch?: UseFormWatch<FieldValues> | string;
+  bold?: boolean;
+  small?: boolean;
+  errColor?: boolean;
+  errors?: string;
 }
 
-const UserInfoInput = ({ type, inputName, watch, register, bold, small }: InputProps) => {
+const UserInfoInput = ({ type, inputName, watch, register, bold, small, errColor, errors }: InputProps) => {
   const [isFocus, setIsFocus] = useState(false);
   return (
-    <InputBox>
-      <CurrentInputName className={isFocus ? "high" : ""} small={small ?? false}>{inputName}</CurrentInputName>
-      <Input small={small ?? false} bold={bold ?? false} type={type} {...register}  onFocus={()=>{setIsFocus(true)}} onBlur={()=>{!watch && setIsFocus(false)}} />
-    </InputBox>
+    <React.Fragment>
+      <InputBox errColor={errColor}>
+        <CurrentInputName className={isFocus ? 'high' : ''} small={small ?? false}>
+          {inputName}
+        </CurrentInputName>
+        <Input
+          small={small ?? false}
+          bold={bold ?? false}
+          type={type}
+          {...register}
+          onFocus={() => {
+            setIsFocus(true);
+          }}
+          onBlur={() => {
+            !watch && setIsFocus(false);
+          }}
+        />
+      </InputBox>
+      {errors && (
+        <ErrMsgBox errColor={errColor}>
+          {errColor ? <ExclamationDiamondFill/> : <ExclamationCircleFill/>}
+          <ErrMsg>{errors}</ErrMsg>
+        </ErrMsgBox>
+      )}
+    </React.Fragment>
   );
 };
 
 export default UserInfoInput;
 
-const InputBox = styled.div`
-  position:relative;
+const InputBox = styled.div<{ errColor?: boolean }>`
+  position: relative;
   width: 100%;
   border-radius: 5px;
-  border: 1px solid #000;
-  overflow:hidden;
-  background:rgba(188,188,188,.1);
-  transition:background .3s;
-  &:focus-within{
-    background:rgba(255,255,255,1);
+  border: 2px solid transparent;
+  overflow: hidden;
+  background: rgba(188, 188, 188, 0.1);
+  transition: background 0.3s, border 0.3s;
+  & + & {
+    margin-top: ${({ theme }) => theme.rem.p20};
   }
+  &:focus-within {
+    background: rgba(255, 255, 255, 1);
+    border: 2px solid #555;
+  }
+  ${({ errColor }) =>
+    errColor &&
+    css`
+      background: rgba(255, 109, 96, 0.1);
+      border: 2px solid rgba(255, 109, 96, 0.1);
+      &:focus-within {
+        border: 2px solid rgba(255, 109, 96, 0.5);
+      }
+      span {
+        color: #FF6D60;
+        &.high{
+          color: #FF6D60;
+        }
+      }
+    `}
 `;
 
-const Input = styled.input<{bold : boolean, small: boolean}>`
-  position:relative;
-  z-index:2;
+const Input = styled.input<{ bold: boolean; small: boolean }>`
+  position: relative;
+  z-index: 2;
   width: 100%;
-  line-height: ${({theme})=>theme.rem.p40};
-  padding : ${({theme})=>theme.rem.p28} ${({theme})=>theme.rem.p12} ${({theme})=>theme.rem.p10};
-  font-size: ${({theme})=>theme.rem.p20};
-  background:rgba(0,0,0,0);
+  line-height: ${({ theme }) => theme.rem.p40};
+  padding: ${({ theme }) => theme.rem.p28} ${({ theme }) => theme.rem.p12} ${({ theme }) => theme.rem.p10};
+  font-size: ${({ theme }) => theme.rem.p20};
+  background: rgba(0, 0, 0, 0);
   border-radius: 5px;
-  ${({bold}) => bold && css`
-    font-weight:bold;
-  `}
-  ${({small, theme}) => small && css`
-    line-height: ${theme.rem.p28};
-    padding : ${theme.rem.p22} ${theme.rem.p12} ${theme.rem.p10};
-    font-size: ${theme.rem.p16};
-  `}
-
+  outline: none;
+  ${({ bold }) =>
+    bold &&
+    css`
+      font-weight: bold;
+    `}
+  ${({ small, theme }) =>
+    small &&
+    css`
+      line-height: ${theme.rem.p28};
+      padding: ${theme.rem.p22} ${theme.rem.p12} ${theme.rem.p10};
+      font-size: ${theme.rem.p16};
+    `}
 `;
 
-const CurrentInputName = styled.span<{ small: boolean}>`
-  position:absolute;
-  z-index:1;
+const CurrentInputName = styled.span<{ small: boolean }>`
+  position: absolute;
+  z-index: 1;
   top: 50%;
   left: 6%;
   transform: translateY(-50%);
-  letter-spacing:1px;
-  transition: .3s;
+  letter-spacing: 1px;
+  transition: 0.3s;
   font-weight: bold;
-  ${({small, theme}) => small && css`
-  font-size : 14px;
-`}
-  &.high{
-    z-index:3;
+  color: #acacac;
+  ${({ small }) =>
+    small &&
+    css`
+      font-size: 14px;
+    `}
+  &.high {
+    z-index: 3;
     top: 25%;
     left: 3%;
-    font-size : 13px;
+    font-size: 13px;
     pointer-events: none;
-    ${({small, theme}) => small && css`
-      font-size : 12px;
-    `}
+    color: #232323;
+    ${({ small }) =>
+      small &&
+      css`
+        font-size: 12px;
+      `}
   }
+`;
+
+const ErrMsgBox = styled(OnlyAlignCenterFlex)<{ errColor?: boolean }>`
+  margin: 6px 0 30px;
+  svg{
+    width: 1em;
+    margin-right: 6px;
+    color: #00C4FF;
+  }
+  ${({ errColor }) =>
+    errColor &&
+    css`
+      color: #FF6D60;
+      svg{
+        color: inherit;
+      }
+    `}
+`;
+const ErrMsg = styled.div`
+
 `;
