88150df22d16dc42f23a1b56278ea4bb9e394dcf
KimKW1007 <face676934@gmail.com>
2023-06-28 08:08:12 +0900

    refactor : miniUserInfo

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -1,96 +1,101 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import logo_black from "@images/black.svg"
-import { OnlyAlignCenterFlex } from '@components/common/CommonFlex'
-import { ChevronDown, ChevronUp } from '@styled-icons/entypo'
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import logo_black from '@images/black.svg';
+import { OnlyAlignCenterFlex } from '@components/common/CommonFlex';
+import { ChevronDown, ChevronUp } from '@styled-icons/entypo';
+import { userInfomation } from '@src/atoms/atoms';
+import { useRecoilState } from 'recoil';
+import LoginSignUpBox from './LoginSignUpBox';
+import UserMiniProfile from './UserMiniProfile';
+
 export const Header = () => {
   const [isCategoryOn, setIsCategoryOn] = useState(false);
+  const [userInfo, setUserInfo] = useRecoilState(userInfomation);
+  const [isMount, setIsMount] = useState<boolean>(false);
+  useEffect(()=>{
+    setIsMount(true);
+  },[])
+
   return (
     <HeaderBox>
       <HeaderInnerBox>
         <LogoAndCategoryBox>
-          <CategoryBtn onClick={()=>{setIsCategoryOn(prev => !prev)}}>
-              CATEGORY
-              {isCategoryOn ? <UpDirection/> : <DownDirection/>}
+          <CategoryBtn
+            onClick={() => {
+              setIsCategoryOn((prev) => !prev);
+            }}>
+            CATEGORY
+            {isCategoryOn ? <UpDirection /> : <DownDirection />}
           </CategoryBtn>
           <LogoBox>
-            <Link href={"/"} title='K - Blog'>
+            <Link href={'/'} title="K - Blog">
               <Image src={logo_black} alt={'로고'}></Image>
             </Link>
           </LogoBox>
         </LogoAndCategoryBox>
         <BtnBox>
-          <LoginSignUpText href={'/login'} title='로그인'>로그인</LoginSignUpText>
-          <ShortLine/>
-          <LoginSignUpText href={'/signup'} className='rightLine' title='회원가입'>회원가입</LoginSignUpText>
+          {isMount && userInfo ? (
+            <UserMiniProfile/>
+          ) : (
+            <LoginSignUpBox/>
+          )}
         </BtnBox>
       </HeaderInnerBox>
     </HeaderBox>
-  )
-}
+  );
+};
 
 const HeaderBox = styled.header`
-  width:100%;
-  height:${({theme})=> theme.rem.p60};
-  padding: 0 ${({theme})=> theme.rem.p30};
-  box-shadow: 0 0 20px  #eee;
-`
+  width: 100%;
+  height: ${({ theme }) => theme.rem.p70};
+  padding: 0 ${({ theme }) => theme.rem.p30};
+  box-shadow: 0 0 20px #eee;
+`;
 const HeaderInnerBox = styled(OnlyAlignCenterFlex)`
-  width:100%;
-  height:100%;
-  padding: 0 ${({theme})=> theme.rem.p30};
-  justify-content:space-between;
-`
-const LogoBox = styled(OnlyAlignCenterFlex)`
-  width: ${({theme})=> theme.rem.p50};
+  width: 100%;
   height: 100%;
-  img{
-    max-width:100%;
+  padding: 0 ${({ theme }) => theme.rem.p30};
+  justify-content: space-between;
+`;
+const LogoBox = styled(OnlyAlignCenterFlex)`
+  width: ${({ theme }) => theme.rem.p50};
+  height: 100%;
+  img {
+    max-width: 100%;
   }
-`
+`;
 
 const BtnBox = styled(OnlyAlignCenterFlex)`
   font-size: 12px;
-`
-const LoginSignUpText = styled(Link)`
-  margin: 0 ${({theme})=> theme.rem.p10};
-`
-
-const ShortLine = styled.i`
-  display: inline-block;
-  width: 1px;
-  height: 1em;
-  background : black;
-`
+`;
 
 const LogoAndCategoryBox = styled.div`
   display: inline-flex;
-  height:100%;
+  height: 100%;
   align-items: center;
-`
+`;
 
 const CategoryBtn = styled.button`
-  display:flex;
+  display: flex;
   align-items: center;
-  justify-content:space-between;
+  justify-content: space-between;
   width: 8rem;
-  height: ${({theme})=> theme.rem.p40};
-  margin-right: ${({theme})=> theme.rem.p30};
-  border-bottom:1px solid #000;
-  padding-left : ${({theme})=> theme.rem.p10};
-  text-align:left;
-  font-size:14px;
+  height: ${({ theme }) => theme.rem.p40};
+  margin-right: ${({ theme }) => theme.rem.p30};
+  border-bottom: 1px solid #000;
+  padding-left: ${({ theme }) => theme.rem.p10};
+  text-align: left;
+  font-size: 14px;
   font-family: 'Pretendard-Regular';
   font-weight: bold;
-  background:rgba(0,0,0,0);
-`
+  background: rgba(0, 0, 0, 0);
+`;
 
 const DownDirection = styled(ChevronDown)`
-  width: ${({theme})=> theme.rem.p20};
-`
+  width: ${({ theme }) => theme.rem.p20};
+`;
 const UpDirection = styled(ChevronUp)`
-  width: ${({theme})=> theme.rem.p20};
-`
-
+  width: ${({ theme }) => theme.rem.p20};
+`;
