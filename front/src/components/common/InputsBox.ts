fabfec08599f1df3db1a1a307439698d1b68767e
KimKW1007 <face676934@gmail.com>
2023-06-27 18:51:32 +0900

    chore: inputsBox 제거

--- front/src/components/common/InputsBox.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const InputsBox = styled.div`
-  width: 100%;
-  div:not(:last-child) {
-    margin-bottom: ${({ theme }) => theme.rem.p20};
-  }
-`;
