9de0dbae018fe8616c8b5d7a7d25f50697357323
KimKW1007 <face676934@gmail.com>
2023-06-28 08:06:07 +0900

    feat : signup finallPage

---
+++ front/src/components/signup/FinallPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components';
+import { EmptyBox } from './FirstPage';
+
+const FinallPage = () => {
+  return (
+    <FinallBox>
+      <FinalText>&#40;준비중&#41; 일단 가입을 축하한다.</FinalText>
+      <EmptyBox/>
+    </FinallBox>
+  )
+}
+
+export default FinallPage
+
+const FinallBox = styled.div`
+
+`
+
+const FinalText =styled.span`
+
+`
+
