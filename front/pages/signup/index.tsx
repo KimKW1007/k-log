1ae708d21d6c1c518456eb94459ebba89bc6fa77
KimKW1007 <face676934@gmail.com>
2023-06-27 18:51:58 +0900

    style : longin/ singup - index

--- front/pages/signup/index.tsx
+++ front/pages/signup/index.tsx
@@ -17,12 +17,14 @@ const SignUpPage: NextPage = () => {
 export default SignUpPage;
 const SignupWrap = styled(OnlyAlignCenterFlex)`
   width: 100%;
-  padding:${({ theme }) => theme.rem.p70}  ${({ theme }) => theme.rem.p100};
+  height:100%;
+  padding:${({ theme }) => theme.rem.p100}  ${({ theme }) => theme.rem.p100};
 `;
 
 const SignupInnerBox = styled.div`
   // 550px
   width: 36rem;
+  height:100%;
   border: 1px solid #000;
   padding: ${({ theme }) => theme.rem.p80} ${({ theme }) => theme.rem.p70} 0;
   background: #fefefe;
