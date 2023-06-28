4c634769738553e96b32efdb194682560eb2a986
KimKW1007 <face676934@gmail.com>
2023-06-28 11:31:25 +0900

    feat : seasonbg - 계절에 따른 배경 변경

--- front/pages/login/index.tsx
+++ front/pages/login/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next';
 import LoginForm from '@components/login/LoginForm';
 import { AllCenterFlex, OnlyAlignCenterFlex } from '@components/common/CommonFlex';
 import styled from 'styled-components';
+import { AuthSeasonBg } from '@components/login/SeasonBg';
 
 const LoginPage: NextPage = () => {
   return (
@@ -20,12 +21,14 @@ const LoginWrap = styled(AllCenterFlex)`
   width: 100%;
   height:100%;
   padding:${({theme})=>theme.rem.p70}  ${({theme})=>theme.rem.p50} ;
+  background : url(${AuthSeasonBg()}) center center no-repeat;
+  background-size:  cover;
 `;
 
 const LoginInnerBox = styled.div`
   // 550px
   width: 36rem;
-  border: 1px solid #000;
   padding:${({theme})=>theme.rem.p80} ${({theme})=>theme.rem.p70} 0;
   background: #fefefe;
+  box-shadow: 25px 25px 18px 10px rgba(0,0,0,.4);
 `;
