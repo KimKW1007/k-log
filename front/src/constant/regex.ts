4ec7b4c223d70198b6a4ae33e04cc88ae9618f7d
KimKW1007 <face676934@gmail.com>
2023-06-28 21:04:26 +0900

    chore : change EMAIL_REGEX /  add NAME_REGEX

--- front/src/constant/regex.ts
+++ front/src/constant/regex.ts
@@ -1,3 +1,4 @@
-export const EMAIL_REGEX = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+export const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export const ACCOUNT_ID_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9]).{4,24}$/;
 export const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$/;;
+export const NAME_REGEX = /^[가-힣a-zA-Z]{2,4}$/;
