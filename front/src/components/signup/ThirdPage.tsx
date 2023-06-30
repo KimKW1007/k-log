47573c2ebd0f73bfe3d90cd4cc4a298641af9fd3
KimKW1007 <face676934@gmail.com>
2023-06-30 20:31:34 +0900

    refactor : password/confirmPw => bundlePasswords 및 function checkSamePassword 위치 변경

--- front/src/components/signup/ThirdPage.tsx
+++ front/src/components/signup/ThirdPage.tsx
@@ -6,19 +6,12 @@ import { InputListBox } from '@components/login/LoginForm';
 import { RegisterPagesProps } from '@src/types/register';
 import { errMsg } from 'utils/singupThirdErrMsg';
 import { errorFn } from 'utils/singupErrorFn';
+import { checkSamePassword, onChangePasswordValidate } from 'utils/checkSamPassword';
+import BundleOfPasswords from '@components/common/BundleOfPasswords';
 
 
 const ThirdPage = ({ register, watch, setIsAllChecked, errors, setError, clearErrors }: RegisterPagesProps) => {
-  const checkSamePassword = () => {
-    // 2단계 : 두개의 password 입력란이 안 비워져있는지
-    if (watch('confirmPassword') && watch('password')!.length >= 8 && watch('confirmPassword')!.length >= 1) {
-      // 3단계 : 두개의 password가 같은지
-      if (watch('password') === watch('confirmPassword')) {
-        return '비밀번호가 일치합니다.';
-      }
-      return '비밀번호가 일치하지 않습니다.';
-    }
-  };
+ 
 
   useEffect(() => {
     if (watch('userId') && watch('password') && watch('confirmPassword')) {
@@ -48,51 +41,7 @@ const ThirdPage = ({ register, watch, setIsAllChecked, errors, setError, clearEr
         errors={errorFn(errMsg['userIdRegexMsg'], errors?.userId)}
         errColor={Boolean(errors?.userId?.message)}
       />
-      <UserInfoInput
-        small
-        type="password"
-        inputName="비밀번호"
-        register={register('password', {
-          required: errMsg['passwordMinLength'],
-          minLength: {
-            value: 8,
-            message: errMsg['passwordMinLength']
-          },
-          validate: {
-            checkPasswordValidate: (value) => PASSWORD_REGEX.test(value!) || errMsg['passwordRegexMsg']
-          },
-          onChange: () => {
-            if (watch('confirmPassword')!.length >= 8) {
-              if (watch('password') !== watch('confirmPassword')) {
-                // 실질적으로 보이진 않고 에러관련 boolean으로 사용
-                setError('confirmPassword', {
-                  type: 'custom',
-                  message: '비밀번호가 일치하지 않습니다.'
-                });
-              } else {
-                clearErrors(['password', 'confirmPassword']);
-              }
-            }
-          }
-        })}
-        watch={watch('password')}
-        errColor={Boolean(errors?.password?.message)}
-      />
-      <UserInfoInput
-        small
-        type="password"
-        inputName="비밀번호 확인"
-        register={register('confirmPassword', {
-          required: errMsg['passwordMinLength'],
-          validate: {
-            // 실질적으로 보이진 않고 에러관련 boolean으로 사용
-            checkPasswordValidate: (value) => watch('password') === value || errMsg['passwordRegexMsg']
-          }
-        })}
-        watch={watch('confirmPassword')}
-        errors={checkSamePassword() || errorFn(errMsg['passwordRegexMsg'], errors?.password, errors?.confirmPassword)}
-        errColor={Boolean(errors?.confirmPassword?.message)}
-      />
+      <BundleOfPasswords register={register} watch={watch} errors={errors} setError={setError} clearErrors={clearErrors} />
     </InputListBox>
   );
 };
