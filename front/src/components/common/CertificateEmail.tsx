0a06e04583b0c1de76ee06fab5cd9768677aef7e
KimKW1007 <face676934@gmail.com>
2023-06-30 20:33:36 +0900

    chore : 불필요한 코드 제거

--- front/src/components/common/CertificateEmail.tsx
+++ front/src/components/common/CertificateEmail.tsx
@@ -3,15 +3,18 @@ import { InputListBox } from '@components/login/LoginForm';
 import EmailInput from '@components/common/EmailInput';
 import { RegisterPagesProps } from '@src/types/register';
 import { useMutation } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import customApi from 'utils/customApi';
 import { CertificateEmailProps } from '@src/types/CertificateEmail';
+import { inputResetBoolean } from '@src/atoms/atoms';
+import { useRecoilState } from 'recoil';
 
 
 
 const CertificateEmail = ({ small, register, watch, errors, setError, setValue, clearErrors, setIsPassCertificate, isPassCertificate }: CertificateEmailProps) => {
   const [isComplete, setIsComplete] = useState(false);
+  const [resetState, setResetState] = useRecoilState(inputResetBoolean);
 
   const { postApi: certificatePostApi } = customApi('/find/sendEmail');
   const { mutate, isLoading, isSuccess } = useMutation(certificatePostApi, {
@@ -20,7 +23,6 @@ const CertificateEmail = ({ small, register, watch, errors, setError, setValue, 
     },
     onSuccess(data, variables, context) {
       setIsComplete(true);
-      setIsPassCertificate!(false);
     }
   });
 
@@ -38,10 +40,12 @@ const CertificateEmail = ({ small, register, watch, errors, setError, setValue, 
     mutate({ userEmail: watch('userEmail') });
     setValue!('token', '');
     clearErrors('token');
-    if (isSuccess) {
-      setIsPassCertificate!(false);
-    }
   };
+
+  useEffect(()=>{
+    setIsComplete(false);
+  },[resetState])
+
   return (
     <>
       <EmailInput
@@ -50,10 +54,10 @@ const CertificateEmail = ({ small, register, watch, errors, setError, setValue, 
         watch={watch('userEmail')}
         errors={errors?.userEmail?.message}
         errColor={Boolean(errors?.userEmail?.message)}
-        isSuccess={isSuccess}
         isLoading={isLoading}
         certificateEmail={certificateEmail}
         isPassCertificate={isPassCertificate}
+        isComplete={isComplete}
       />
       {isComplete && (
         <Certificate
