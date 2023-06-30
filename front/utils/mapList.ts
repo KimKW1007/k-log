544a2934dfb2ab0d22f116557301aa50900f6dd6
KimKW1007 <face676934@gmail.com>
2023-06-30 17:21:08 +0900

    feat : mapList - findList

--- front/utils/mapList.ts
+++ front/utils/mapList.ts
@@ -17,3 +17,18 @@ export const tabList: ListTypes[] = [
   { id: 'tabId', text: '아이디' },
   { id: 'tabPassword', text: '비밀번호' }
 ];
+
+export const findList: ListTypes[] = [
+  {
+    id: 'tabId',
+    findText: '아이디 찾기',
+    loginText: '로그인 하러가기',
+    singupText: '회원가입 하러가기'
+  },
+  {
+    id: 'tabPassword',
+    findText: '비밀번호 찾기',
+    loginText: '로그인 하러가기',
+    changePassword: '비밀번호 변경하기'
+  }
+];
