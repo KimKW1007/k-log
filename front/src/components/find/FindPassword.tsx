c17b157458a31939e173d386f3576231da3f6601
KimKW1007 <face676934@gmail.com>
2023-06-30 20:32:51 +0900

    feat : bundlepassword 추가

--- front/src/components/find/FindPassword.tsx
+++ front/src/components/find/FindPassword.tsx
@@ -1,47 +1,69 @@
-import CertificateEmail from '@components/common/CertificateEmail'
-import UserInfoInput from '@components/common/UserInfoInput'
-import { InputListBox } from '@components/login/LoginForm'
-import { ACCOUNT_ID_REGEX } from '@src/constant/regex'
-import { CertificateEmailProps } from '@src/types/CertificateEmail'
-import React from 'react'
-import { errorFn } from 'utils/singupErrorFn'
-import { errMsg } from 'utils/singupThirdErrMsg'
+import BundleOfPasswords from '@components/common/BundleOfPasswords';
+import CertificateEmail from '@components/common/CertificateEmail';
+import FinallPage from '@components/common/FinallPage';
+import UserInfoInput from '@components/common/UserInfoInput';
+import { InputListBox } from '@components/login/LoginForm';
+import { ACCOUNT_ID_REGEX, PASSWORD_REGEX } from '@src/constant/regex';
+import { CertificateEmailProps } from '@src/types/CertificateEmail';
+import React from 'react';
+import { checkSamePassword, onChangePasswordValidate } from 'utils/checkSamPassword';
+import { errorFn } from 'utils/singupErrorFn';
+import { errMsg } from 'utils/singupThirdErrMsg';
 
-const FindPassword = ({ register, watch, errors, setError, setValue, clearErrors, setIsPassCertificate, isPassCertificate, isClickFindBtn, userIds }: CertificateEmailProps) => {
+const FindPassword = ({
+  register,
+  watch,
+  errors,
+  setError,
+  setValue,
+  clearErrors,
+  setIsPassCertificate,
+  isPassCertificate,
+  isClickFindBtn,
+  isSuccessChangePassword
+}: CertificateEmailProps) => {
   return (
     <InputListBox>
-      <UserInfoInput
-        small
-        bold
-        type="text"
-        inputName="아이디"
-        register={register('userId', {
-          required: '값을 입력해주세요',
-          minLength: {
-            value: 4,
-            message: errMsg['userIdMinLength']
-          },
-          validate: {
-            checkAccountId: (value) => ACCOUNT_ID_REGEX.test(value!) || errMsg['userIdRegexMsg']
-          }
-        })}
-        watch={watch('userId')}
-        errors={errorFn(errMsg['userIdRegexMsg'], errors?.userId)}
-        errColor={Boolean(errors?.userId?.message)}
-      />
-      <CertificateEmail
-        small
-        register={register}
-        watch={watch}
-        errors={errors}
-        setError={setError}
-        setValue={setValue}
-        clearErrors={clearErrors}
-        setIsPassCertificate={setIsPassCertificate}
-        isPassCertificate={isPassCertificate}
-      />
+      {isSuccessChangePassword && <FinallPage />}
+      {isSuccessChangePassword ||
+        (isClickFindBtn ? (
+          <BundleOfPasswords register={register} watch={watch} errors={errors} setError={setError} clearErrors={clearErrors} />
+        ) : (
+          <>
+            <UserInfoInput
+              small
+              bold
+              type="text"
+              inputName="아이디"
+              register={register('userId', {
+                required: '값을 입력해주세요',
+                minLength: {
+                  value: 4,
+                  message: errMsg['userIdMinLength']
+                },
+                validate: {
+                  checkAccountId: (value) => ACCOUNT_ID_REGEX.test(value!) || errMsg['userIdRegexMsg']
+                }
+              })}
+              watch={watch('userId')}
+              errors={errorFn(errMsg['userIdRegexMsg'], errors?.userId)}
+              errColor={Boolean(errors?.userId?.message)}
+            />
+            <CertificateEmail
+              small
+              register={register}
+              watch={watch}
+              errors={errors}
+              setError={setError}
+              setValue={setValue}
+              clearErrors={clearErrors}
+              setIsPassCertificate={setIsPassCertificate}
+              isPassCertificate={isPassCertificate}
+            />
+          </>
+        ))}
     </InputListBox>
-  )
-}
+  );
+};
 
-export default FindPassword
+export default FindPassword;
