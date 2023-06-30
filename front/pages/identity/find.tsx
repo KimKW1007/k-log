aca847f135b3a141e446f41ce0a2f5698fb267f1
KimKW1007 <face676934@gmail.com>
2023-07-01 08:36:32 +0900

    style : findTab

--- front/pages/identity/find.tsx
+++ front/pages/identity/find.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next';
 import React, { MouseEvent, useEffect, useState } from 'react';
 import { inputResetBoolean } from '@src/atoms/atoms';
 import { useRecoilState } from 'recoil';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { ListTypes, findList, tabList } from 'utils/mapList';
 
 const FindPage: NextPage = () => {
@@ -12,23 +12,25 @@ const FindPage: NextPage = () => {
   const [isOnPasswordTab, setIsOnPasswordTab] = useState(false);
   const [textById, setTextById] = useState<ListTypes>();
 
-  const [resetState, setResetState] = useRecoilState(inputResetBoolean)
+  const [resetState, setResetState] = useRecoilState(inputResetBoolean);
 
-  const onClickChangeTab=(id : string)=>()=>{
-    id === "tabId" ? setIsOnPasswordTab(false) : setIsOnPasswordTab(true)
-    setTextById(findList.find(x => x.id === id))
-    setResetState(prev => !prev)
-  }
-  useEffect(()=>{
-    setTextById(findList[0])
-  },[])
+  const onClickChangeTab = (id: string) => () => {
+    id === 'tabId' ? setIsOnPasswordTab(false) : setIsOnPasswordTab(true);
+    setTextById(findList.find((x) => x.id === id));
+    setResetState((prev) => !prev);
+  };
+  useEffect(() => {
+    setTextById(findList[0]);
+  }, []);
 
   return (
     <FindPageWrap>
       <FindInnerBox>
         <SelectTabBox>
-          {tabList.map(({id, text}) =>(
-            <FindTabBox id={id}><FindBtn onClick={onClickChangeTab(id)}>{text} 찾기</FindBtn></FindTabBox>
+          {tabList.map(({ id, text }, idx: number) => (
+            <FindTabBox id={id} >
+              <FindBtn onClick={onClickChangeTab(id)} currentId={id} textById={textById?.id}>{text} 찾기</FindBtn>
+            </FindTabBox>
           ))}
         </SelectTabBox>
         <FormBox>
@@ -48,30 +50,46 @@ const FindPageWrap = styled(AllCenterFlex)`
 
 const FindInnerBox = styled.div`
   width: 33rem;
-  border: 1px solid #000;
 `;
 
 const SelectTabBox = styled.div`
-  width:100%;
-  display:flex;
+  width: 100%;
+  display: flex;
+`;
+
+const FindBtn = styled.button<{currentId : string; textById ?: string;}>`
+  width: 100%;
+  padding: 16px 0;
+  font-weight: bold;
+  font-size: 18px;
+  border-radius: 8px 8px 0 0;
+  border-bottom: 1px solid transparent;
+  background: #232323;
+  color: #e5e5e5;
+  transform : scale(1);
+  transition: .3s;
+  ${({currentId, textById}) => currentId !== textById && css`
+  transform : scale(.99);
+    background: #f5f5f5;
+    color: #bbb;
+    border-bottom: 1px solid #b5b5b5;
+    ${currentId === 'tabId' && css`
+    
+    `}
+  `}
+
 `;
 
 const FindTabBox = styled.div`
-  flex:1;
-  &+&{
-    border-left : 1px solid #000;
-  }
-`
+  flex: 1;
+`;
 
-const FindBtn = styled.button`
-  width:100%;
-  padding: 16px 0;
-  font-weight:bold;
-  font-size :18px;
-`
 
-const FormBox =styled.div`
-height: 500px;
-  padding:${({theme})=>theme.rem.p50} ${({theme})=>theme.rem.p40} ${({theme})=>theme.rem.p60};
-  background: #fefefe;  
-`
+
+const FormBox = styled.div`
+  height: 500px;
+  padding: ${({ theme }) => theme.rem.p50} ${({ theme }) => theme.rem.p40} ${({ theme }) => theme.rem.p60};
+  background: #fefefe;
+  border: 1px solid #e5e5e5;
+  border-top: 0;
+`;
