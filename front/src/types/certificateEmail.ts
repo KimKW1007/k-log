b059ad892ffa754984109721213dbde9637bcd09
KimKW1007 <face676934@gmail.com>
2023-06-30 20:30:10 +0900

    chore : certificateEmailProps isSuccessChangePassword 추가

--- front/src/types/certificateEmail.ts
+++ front/src/types/certificateEmail.ts
@@ -4,5 +4,6 @@ import { User } from "./user";
 export interface CertificateEmailProps extends Omit<RegisterPagesProps, 'setIsAllChecked'> {
   small?: boolean;
   isClickFindBtn ?:boolean;
+  isSuccessChangePassword ?:boolean;
   userIds ?: User[];
 }
