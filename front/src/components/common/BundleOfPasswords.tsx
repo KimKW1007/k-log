63543ef76a6049dd89d99e313c4ef350a020f336
KimKW1007 <face676934@gmail.com>
2023-07-03 14:05:32 +0900

    refactor : add validate / add errorBox

--- front/src/components/common/BundleOfPasswords.tsx
+++ front/src/components/common/BundleOfPasswords.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import UserInfoInput from './UserInfoInput';
+import UserInfoInput, { ErrMsg, ErrMsgBox } from './UserInfoInput';
 import { errMsg } from '@utils/singupThirdErrMsg';
 import { PASSWORD_REGEX } from '@constant/regex';
 import { checkSamePassword, onChangePasswordValidate } from '@utils/checkSamPassword';
 import { errorFn } from '@utils/singupErrorFn';
 import { RegisterPagesProps } from '@src/types/register';
+import { ExclamationCircleFill, ExclamationDiamondFill } from '@styled-icons/bootstrap';
 
 const BundleOfPasswords = ({ register, watch, errors, setError, clearErrors }: Omit<RegisterPagesProps, 'setIsAllChecked'>) => {
+
+  const Is_ErrColor = Boolean(errors?.password?.message) || Boolean(errors?.confirmPassword?.message)
   return (
     <>
       <UserInfoInput
@@ -33,15 +36,25 @@ const BundleOfPasswords = ({ register, watch, errors, setError, clearErrors }: O
         inputName="비밀번호 확인"
         register={register('confirmPassword', {
           required: errMsg['passwordMinLength'],
+          minLength: {
+            value: 8,
+            message: errMsg['passwordMinLength']
+          },
           validate: {
             // 실질적으로 보이진 않고 에러관련 boolean으로 사용
-            checkPasswordValidate: (value) => watch('password') === value || errMsg['passwordRegexMsg']
+            checkPasswordValidate: (value) => watch('password') === value || errMsg['passwordRegexMsg'],
+            checkPasswordValidate2: (value) => PASSWORD_REGEX.test(value!) || errMsg['passwordRegexMsg']
           }
         })}
         watch={watch('confirmPassword')}
-        errors={checkSamePassword(watch) || errorFn(errMsg['passwordRegexMsg'], errors?.password, errors?.confirmPassword)}
         errColor={Boolean(errors?.confirmPassword?.message)}
       />
+      {errors && (
+        <ErrMsgBox errColor={Is_ErrColor}>
+          {Is_ErrColor ? <ExclamationDiamondFill /> : <ExclamationCircleFill />}
+          <ErrMsg>{checkSamePassword(watch) || errorFn(errMsg['passwordRegexMsg'], errors?.password, errors?.confirmPassword)}</ErrMsg>
+        </ErrMsgBox>
+      )}
     </>
   );
 };
