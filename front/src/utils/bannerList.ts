febffe84ed35c0928a51a5f3fdc71d3ee555b6a8
KimKW1007 <face676934@gmail.com>
2023-07-04 15:18:02 +0900

    feat : bannerList

--- front/src/utils/bannerList.ts
+++ front/src/utils/bannerList.ts
@@ -1,10 +1,18 @@
 import winterBg from '@assets/images/winterBg.jpg';
 import fallBg from '@assets/images/fallBg.jpg';
 import springBg from '@assets/images/springBg.jpg';
+import { ListTypes } from './mapList';
 
+//  배경은 3개만
 
 const banner1 = winterBg.src;
 const banner2 = fallBg.src;
 const banner3 = springBg.src;
 
-export {banner1, banner2, banner3 }
+export const banner: ListTypes = {
+  banner1: winterBg.src,
+  banner2: fallBg.src,
+  banner3: springBg.src
+};
+
+export { banner1, banner2, banner3 };
