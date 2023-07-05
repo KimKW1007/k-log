537f07f01cd9efb1602c7365377acf9b3a6fbf25
KimKW1007 <face676934@gmail.com>
2023-07-05 20:33:34 +0900

    test : accountSettingPage 임시 설정

---
+++ front/pages/accountSetting/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import styled,{keyframes} from 'styled-components';
+import type { NextPage } from 'next';
+
+
+const AccountSettingPage: NextPage = () => {
+  return (
+    <div>AccountSettingPage</div>
+  )
+}
+
+export default AccountSettingPage
