b26fe1ba6508267582c100d12d09971d248123b1
KimKW1007 <face676934@gmail.com>
2023-07-06 23:28:39 +0900

    feat : accountSetting Page

--- front/pages/accountSetting/index.tsx
+++ front/pages/accountSetting/index.tsx
@@ -1,12 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import styled,{keyframes} from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 import type { NextPage } from 'next';
-
+import { AllCenterFlex } from '@components/common/CommonFlex';
+import UserInfoSideBar from '@components/sideBar/UserInfoSideBar';
 
 const AccountSettingPage: NextPage = () => {
   return (
-    <div>AccountSettingPage</div>
-  )
-}
+    <SettingWrap>
+      <SettingInnerBox>
+        <SideProfileSettingBox>
+          <UserInfoSideBar></UserInfoSideBar>
+        </SideProfileSettingBox>
+        <UserInfoSettingBox></UserInfoSettingBox>
+      </SettingInnerBox>
+    </SettingWrap>
+  );
+};
 
-export default AccountSettingPage
+export default AccountSettingPage;
+
+const SettingWrap = styled(AllCenterFlex)`
+  width:100%;
+  padding: 100px 30px 0;
+`;
+const SettingInnerBox = styled.div`
+  width: 1200px;
+  height: 500px;
+`;
+
+const UserInfoSettingBox = styled.div`
+  flex-grow: 1;
+`;
+const SideProfileSettingBox = styled.div`
+  width: 360px;
+  border : 1px solid #DDE6ED;
+  border-radius: 30px;
+
+`;
