5b8c3d4d67d03d21c3fce4ba2569d1473b49f25d
KimKW1007 <face676934@gmail.com>
2023-07-08 01:49:30 +0900

    style : AddCategoryBtn

--- front/src/components/category/AddCategoryBtn.tsx
+++ front/src/components/category/AddCategoryBtn.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import styled, { keyframes } from 'styled-components';
 const AddCategoryBtn = ({onClick} : {onClick: () => void}) => {
   return (
-    <AddBtn type="button" onClick={onClick}>+ 새 카테고리 만들기</AddBtn>
+    <AddBtn type="button" onClick={onClick}>새 카테고리 만들기</AddBtn>
   )
 }
 
 export default AddCategoryBtn
 const AddBtn = styled.button`
-
+  width:100%;
+  margin-top: 6px;
+  padding: 5px 0;
+  border-radius: 6px;
+  background: #f2f2f2;
+  color: #acacac;
+  transition: .2s;
+  &:hover{
+    background: #e5e5e5;
+    color: #555;
+  }
 `;
