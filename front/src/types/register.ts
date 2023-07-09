e10330beb8ab8efaaac4bf4fa2d172f6dfbd582e
KimKW1007 <face676934@gmail.com>
2023-07-09 22:21:37 +0900

    refactor  : type/register - 불필요한  type 제거

--- front/src/types/register.ts
+++ front/src/types/register.ts
@@ -1,14 +1,6 @@
-import { FieldErrors, UseFormClearErrors, UseFormRegister, UseFormSetError, UseFormSetValue, UseFormWatch } from "react-hook-form";
-import { RegisterInputs } from "./user";
 
-export interface RegisterPagesProps {
-  register: UseFormRegister<RegisterInputs>;
-  watch: UseFormWatch<RegisterInputs>;
-  setIsAllChecked: React.Dispatch<React.SetStateAction<boolean>>;
-  errors?: FieldErrors<RegisterInputs>;
-  setIsPassCertificate?: React.Dispatch<React.SetStateAction<boolean>>;
-  isPassCertificate?: boolean;
-  setValue?: UseFormSetValue<RegisterInputs>;
-  setError: UseFormSetError<RegisterInputs>;
-  clearErrors: UseFormClearErrors<RegisterInputs>;
+export interface RegisterPageProps {
+  setIsAllChecked: React.Dispatch<React.SetStateAction<boolean>>
+  setIsPassCertificate : React.Dispatch<React.SetStateAction<boolean>>;
+  isPassCertificate : boolean;
 }
