e9b5926d59d27fcf94a590d89deb486a0332240e
KimKW1007 <face676934@gmail.com>
2023-07-09 20:26:11 +0900

    test & style : accountSettingPage

--- front/pages/accountSetting/index.tsx
+++ front/pages/accountSetting/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import type { NextPage } from 'next';
-import { AllCenterFlex } from '@components/common/CommonFlex';
+import { AllCenterFlex, OnlyJustifyCenterFlex } from '@components/common/CommonFlex';
 import UserInfoSideBar from '@components/sideBar/UserInfoSideBar';
+import UserInfoSetting from '@components/user/UserInfoSetting';
 
 const AccountSettingPage: NextPage = () => {
   return (
@@ -11,7 +12,9 @@ const AccountSettingPage: NextPage = () => {
         <SideProfileSettingBox>
           <UserInfoSideBar></UserInfoSideBar>
         </SideProfileSettingBox>
-        <UserInfoSettingBox></UserInfoSettingBox>
+        <UserInfoSettingBox>
+          {<UserInfoSetting></UserInfoSetting>}
+        </UserInfoSettingBox>
       </SettingInnerBox>
     </SettingWrap>
   );
@@ -19,20 +22,27 @@ const AccountSettingPage: NextPage = () => {
 
 export default AccountSettingPage;
 
-const SettingWrap = styled(AllCenterFlex)`
+const SettingWrap = styled(OnlyJustifyCenterFlex)`
   width:100%;
-  padding: 100px 30px;
+  padding: ${({theme}) => theme.rem.p100} ${({theme}) => theme.rem.p30};
 `;
 const SettingInnerBox = styled.div`
   width: 1300px;
+  display:flex;
+  column-gap: ${({theme}) => theme.rem.p30};
 `;
 
 const UserInfoSettingBox = styled.div`
   flex-grow: 1;
+  border : 1px solid #DDE6ED;
+  border-radius: 30px;
+  overflow:hidden;
+  padding: ${({theme}) => theme.rem.p60} ${({theme}) => theme.rem.p20};
 `;
 const SideProfileSettingBox = styled.div`
   width: 360px;
   border : 1px solid #DDE6ED;
   border-radius: 30px;
-
+  overflow:hidden;
+  padding: ${({theme}) => theme.rem.p20};
 `;
