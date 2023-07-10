3fea0e1ac7c8d8df623515416a06c2b79eb060eb
KimKW1007 <face676934@gmail.com>
2023-07-10 22:41:31 +0900

    feat : customApi - accessToken

--- front/src/utils/customApi.ts
+++ front/src/utils/customApi.ts
@@ -1,59 +1,49 @@
-import axios from "axios";
+import axios from 'axios';
+
+
+const accessToken = typeof window !== 'undefined' ? sessionStorage.getItem('jwtToken') : null;
 
 const axiosBase = axios.create({
-  headers:{
-    "Content-Type": "application/json",
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: "Bearer " + accessToken,
   }
-})
+});
 const baseApi = () => {
   axiosBase.interceptors.response.use(
-    response => response,
-    error => {
+    (response) => response,
+    (error) => {
       throw error;
-    },
+    }
   );
 
   return axiosBase;
 };
-axiosBase.defaults.baseURL = 'http://localhost:5000'
+axiosBase.defaults.baseURL = 'http://localhost:5000';
+axiosBase.defaults.withCredentials = true;
+
+
 
 export default function customApi<T = any>(url: string) {
   const postApi = async (data: T) => {
-    const result = await baseApi().post(url, data,
-      { withCredentials: true });
+    const result = await baseApi().post(url, data);
     return result.data;
   };
   const getApi = async () => {
-    const result = await baseApi().get(url,{
-      headers:{
-        "Authorization" : `Bearer ${sessionStorage.getItem("jwtToken")}`
-      }
-    });
+    const result = await baseApi().get(url);
     return result.data;
   };
 
   const deleteApi = async (data: T) => {
-    const result = await baseApi().delete(url, { data,
-      headers:{
-        "Authorization" : `Bearer ${sessionStorage.getItem("jwtToken")}`
-      }
-    });
+    const result = await baseApi().delete(url, { data });
     return result.data;
   };
   const putApi = async (data: T) => {
-    const result = await baseApi().put(url, data,{
-      headers:{
-        "Authorization" : `Bearer ${sessionStorage.getItem("jwtToken")}`
-      }
-    });
+    const result = await baseApi().put(url, data);
     return result.data;
   };
   const patchApi = async (data: T) => {
-    const result = await baseApi().patch(url, data,{
-      headers:{
-        "Authorization" : `Bearer ${sessionStorage.getItem("jwtToken")}`
-      }
-    });
+    const result = await baseApi().patch(url, data);
     return result.data;
   };
   return { postApi, getApi, deleteApi, putApi, patchApi };
