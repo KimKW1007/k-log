3b80c7c56f2299cb280c88a5198a0544c85e8d55
KimKW1007 <face676934@gmail.com>
2023-07-10 22:36:23 +0900

    feat : EmailInput - RewriteBtn

--- front/src/components/common/EmailInput.tsx
+++ front/src/components/common/EmailInput.tsx
@@ -1,61 +1,69 @@
-import { CertificateBtn, CertificateBtnBox, CurrentInputName, ErrMsg, ErrMsgBox, Input, InputBox } from '@components/common/UserInfoInput';
+import { CurrentInputName, ErrMsg, ErrMsgBox, Input, InputBox } from '@components/common/UserInfoInput';
 import { EMAIL_REGEX } from '@constant/regex';
 import { RegisterInputs, UserInfoInputProps } from '@src/types/user';
 import { ExclamationCircleFill, ExclamationDiamondFill } from '@styled-icons/bootstrap';
 import React, { useEffect, useState } from 'react';
 import { UseFormRegister, useFormContext } from 'react-hook-form';
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import ErrorMsgBox from './error/ErrorMsgBox';
+import {Reload} from '@styled-icons/ionicons-solid/Reload';
+import CertificateBtnBox from './CertificateBtnBox';
+
 
 interface EmailProps extends Omit<UserInfoInputProps, 'register' | 'type' | 'inputName'> {
   inputName?: string;
   isComplete: boolean;
   isLoading: boolean;
   certificateEmail: () => void;
-  isPassCertificate?: boolean;
+  isPassCertificate : boolean;
+  onClickRewrite: () => void
 }
 
-const EmailInput = ({ inputName = '이메일', watch,  small = false, errColor, errors, isComplete, isLoading = false, certificateEmail, isPassCertificate }: EmailProps) => {
+const EmailInput = ({ inputName = '이메일', watch,  small = false, errColor, errors, isComplete, isLoading = false, certificateEmail, isPassCertificate, onClickRewrite }: EmailProps) => {
   const [isFocus, setIsFocus] = useState(false);
-  const { register } = useFormContext();
-
+  const { register, setValue } = useFormContext();
   useEffect(() => {
     watch ? setIsFocus(true) : setIsFocus(false);
   }, [watch]);
   return (
     <React.Fragment>
       <OuterBox>
-        <InputBox errColor={errColor} className={isLoading || isComplete ? 'certified' : ''}>
-          <Input
-            small={small}
-            bold={true}
-            isEmail
-            type="text"
-            {...register('userEmail', {
-              required: '값을 입력해주세요',
-              validate: {
-                checkEmailValidate: (value) => EMAIL_REGEX.test(value!) || '이메일 형식에 맞지 않습니다'
-              }
-            })}
-            onFocus={() => {
-              setIsFocus(true);
-            }}
-            onBlur={() => {
-              !watch && setIsFocus(false);
-            }}
-            autoComplete="off"
-          />
-          <CurrentInputName className={isFocus ? 'high' : ''} small={small}>
-            {inputName}
-          </CurrentInputName>
-        </InputBox>
-        {isPassCertificate || (
-          <CertificateBtnBox>
-            <CertificateBtn type="button" onClick={certificateEmail}>
+        <InnerBox isComplete={isComplete}>
+          <InputBox errColor={errColor} className={isLoading || isComplete ? 'certified' : ''}>
+            <Input
+              small={small}
+              bold={true}
+              isEmail
+              type="text"
+              {...register('userEmail', {
+                required: '값을 입력해주세요',
+                validate: {
+                  checkEmailValidate: (value) => EMAIL_REGEX.test(value!) || '이메일 형식에 맞지 않습니다'
+                }
+              })}
+              onFocus={() => {
+                setIsFocus(true);
+              }}
+              onBlur={() => {
+                !watch && setIsFocus(false);
+              }}
+              autoComplete="off"
+            />
+            <CurrentInputName className={isFocus ? 'high' : ''} small={small}>
+              {inputName}
+            </CurrentInputName>
+          </InputBox>
+          {isPassCertificate || (
+          <CertificateBtnBox onClick={certificateEmail}>
               {isLoading ? '전송 중' : isComplete ? '재전송' : '인증번호 받기'}
-            </CertificateBtn>
           </CertificateBtnBox>
         )}
+        </InnerBox>
+        <RewriteBtn type='button' title='재작성' onClick={onClickRewrite} isComplete={isComplete}>
+          <span className='blind'>재작성</span>
+          <Reload></Reload>
+        </RewriteBtn>
+        
       </OuterBox>
       {errors && <ErrorMsgBox errColor={errColor} errors={errors} />}
     </React.Fragment>
@@ -64,8 +72,47 @@ const EmailInput = ({ inputName = '이메일', watch,  small = false, errColor, 
 
 export default EmailInput;
 
+const RewriteDisplayAni = keyframes`
+  100%{
+    opacity: 1;
+  }
+`
+
+const RewriteBtn = styled.button<{isComplete : boolean;}>`
+  position: absolute;
+  right: 0;
+  top: 50%;
+  margin-top: -15px;
+  width: 30px;
+  height: 30px;
+  padding: 5px;
+  border-radius: 50%;
+  background : #898989;
+  box-shadow: 0 0 5px 1px rgba(255,255,255,.4) inset;
+  display:none;
+  opacity: 0;
+  svg{
+    color: #fff;
+    width: 100%;
+  }
+  ${({isComplete}) => isComplete && css`
+  animation: ${RewriteDisplayAni} .3s .1s forwards;
+  display:block;
+`}
+`
+
 const OuterBox = styled.div`
   position: relative;
   width: 100%;
   margin-top: ${({ theme }) => theme.rem.p20};
+  display:flex;
 `;
+
+const InnerBox =styled.div<{isComplete : boolean;}>`
+position: relative;
+width: 100%;
+transition: width .2s;
+${({isComplete}) => isComplete && `
+  width: calc(100% - 40px);
+`}
+`
