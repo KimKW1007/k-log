1446c62a9820aa848bfbe24acd9753682bfe93b2
KimKW1007 <face676934@gmail.com>
2023-07-11 11:41:00 +0900

    chore : LoginErrBox - add handleOpenPopup

--- front/src/components/login/LoginErrBox.tsx
+++ front/src/components/login/LoginErrBox.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Link from 'next/link';
 import styled from 'styled-components';
+import { handleOpenPopup } from '@utils/handleOpenPopup';
 
 const LoginErrBox = () => {
+  const onClickFindIdOrPassword = (e: React.MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>) => {
+    handleOpenPopup('/identity/find','아이디/비밀번호 찾기', 545, 560 )
+    return e.preventDefault();
+  };
   return (
     <ErrMsgBox>
       <p>아이디 또는 비밀번호가 일치하지 않습니다.</p>
-      <p>계정을 찾고 싶으신 경우에는 이 <Link href={'/'}>페이지</Link>를 방문하여 확인해주세요. </p>
+      <p>계정을 찾고 싶으신 경우에는 이 <Link href={'/'} onClick={onClickFindIdOrPassword}>페이지</Link>를 방문하여 확인해주세요. </p>
     </ErrMsgBox>
   )
 }
