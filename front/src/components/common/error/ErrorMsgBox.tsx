afe4de70dc9af593d36c6232e016f20b6996461b
KimKW1007 <face676934@gmail.com>
2023-07-11 21:55:04 +0900

    chore : ErrorMsgBox - isSettingPage => isEditPage

--- front/src/components/common/error/ErrorMsgBox.tsx
+++ front/src/components/common/error/ErrorMsgBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import styled, { css } from 'styled-components';
 import { ExclamationDiamondFill, ExclamationCircleFill } from '@styled-icons/bootstrap';
 import { OnlyAlignCenterFlex } from '../CommonFlex';
@@ -7,21 +7,21 @@ import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
 interface ErrorProps {
   errColor?: boolean;
   errors?: string | FieldError | Merge<FieldError, FieldErrorsImpl<any>>;
-  isSettingPage ?: boolean;
+  isEditPage?: boolean;
 }
 
-const ErrorMsgBox = ({errColor, errors, isSettingPage}:ErrorProps) => {
+const ErrorMsgBox = ({ errColor, errors, isEditPage }: ErrorProps) => {
   return (
-    <ErrMsgBox errColor={errColor} isSettingPage={isSettingPage}>
+    <ErrMsgBox errColor={errColor} isEditPage={isEditPage}>
       {errColor ? <ExclamationDiamondFill /> : <ExclamationCircleFill />}
       <ErrMsg>{`${errors}`}</ErrMsg>
     </ErrMsgBox>
-  )
-}
+  );
+};
 
-export default ErrorMsgBox
+export default ErrorMsgBox;
 
-export const ErrMsgBox = styled(OnlyAlignCenterFlex)<{ errColor?: boolean; isSettingPage ?: boolean }>`
+export const ErrMsgBox = styled(OnlyAlignCenterFlex)<{ errColor?: boolean; isEditPage?: boolean }>`
   margin: 6px 0 30px;
   svg {
     width: 1em;
@@ -37,7 +37,9 @@ export const ErrMsgBox = styled(OnlyAlignCenterFlex)<{ errColor?: boolean; isSet
       }
     `}
 
-  ${({isSettingPage}) => isSettingPage && `
+  ${({ isEditPage }) =>
+    isEditPage &&
+    `
     margin: 6px 0 ;
   `}
 `;
