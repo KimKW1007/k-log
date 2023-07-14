9d80b78cc8d0799369f3d347d549d0c358a2e553
KimKW1007 <face676934@gmail.com>
2023-07-14 19:55:20 +0900

    style : SearchBox - Editform 에서 제외 및 stlye 변경

--- front/src/components/sideBar/SearchBox.tsx
+++ front/src/components/sideBar/SearchBox.tsx
@@ -1,13 +1,13 @@
 import styled, { keyframes } from 'styled-components';
 import React from 'react'
 import {Search} from "@styled-icons/fluentui-system-filled/Search"
-  const SearchBox = ({isEdit = false} :{isEdit?: boolean}) => {
+  const SearchBox = () => {
   return (
     <SearchForm>
       <SearchInputBox >
-        <SearchInput isEdit={isEdit} placeholder='검색어를 입력하세요'/>
+        <SearchInput  placeholder='검색어를 입력하세요'/>
         <SubmitBtnBox>
-          <SubmitBtn isEdit={isEdit}>
+          <SubmitBtn >
             <Search />
           </SubmitBtn>
         </SubmitBtnBox>
@@ -27,58 +27,53 @@ const SearchForm = styled.form`
 const SearchInputBox = styled.div`
   position: relative;
   width:100%;
-  padding: 0 30px;
 `
 
-const SearchInput = styled.input<{isEdit?: boolean}>`
+const SearchInput = styled.input`
   width:100%;
   line-height: 30px;
   font-size: 13px;
   padding: 6px 50px 6px 20px;
-  background:#f2f2f8;
-
+  background:#545454;
   outline: none;
   border-radius: 30px;
+  color: #fff;
+  font-weight: 700;
   &::placeholder{
     font-size: 12px;
-    color: #a5a5a5;
+    color: #fff;
     font-weight:bold;
     letter-spacing: -1px;
   }
-  ${({isEdit}) => isEdit &&`
-    pointer-events:none;
-    background: #454545a1;
-  `}
+
 `
 
 const SubmitBtnBox = styled.div`
   position: absolute;
-  right: 30px;
-  top:0;
+  right: 0;
+  top: 0;
   width: 42px;
   height: 42px;
   border-radius: 30px;
   overflow:hidden;
+  
 `
 
 const SubmitBtn=  styled.button<{isEdit?: boolean}>`
   width:100%;
   height:100%;
   padding: 8px;
-  background:#454545;
+  background:#898989;
+  border-radius: 30px;
   transition: .2s;
+  border : 2px solid transparent;
   svg{
     width:100%;
     color: #fff;
   }
-  ${({isEdit}) => isEdit &&`
-    pointer-events:none;
-    svg{
-      color: #898989;
-    }
-  `}
   &:hover{
-    background:#232323;
+    background:#434343;
+    border : 2px solid #898989;
   }
 
 `
