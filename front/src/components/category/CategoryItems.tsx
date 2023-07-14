cac87bb0b6c1fd61d6b6b3449d5276c2b82940a0
KimKW1007 <face676934@gmail.com>
2023-07-14 19:57:41 +0900

    feat : CategoryItems

---
+++ front/src/components/category/CategoryItems.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import Link from 'next/link';
+import customApi from '@utils/customApi';
+import { useQuery } from '@tanstack/react-query';
+import { GET_ALL_CATEGORY } from '@utils/queryKeys';
+import { CategoryBackProps, SubCategoryBackProps } from './CategoryList';
+
+const CategoryItems = () => {
+  const { getApi } = customApi('/category');
+  const { data, isLoading, isSuccess } = useQuery([GET_ALL_CATEGORY], getApi);
+  return (
+    <>
+      {data &&
+        data.map(({ categoryTitle, subCategories }: CategoryBackProps) => (
+          <CategoryItmeList>
+            <CategoryTitle>
+              <Link prefetch href={`/category/${categoryTitle}`}>
+                {categoryTitle}
+              </Link>
+            </CategoryTitle>
+            {subCategories.map(({ categorySubTitle, id }: SubCategoryBackProps) => (
+              <CategoryItem>
+                <Link prefetch href={`/category/${categoryTitle}/${categorySubTitle}`}>
+                  {categorySubTitle}
+                </Link>
+              </CategoryItem>
+            ))}
+          </CategoryItmeList>
+        ))}
+    </>
+  );
+};
+
+export default CategoryItems;
+
+const CategoryItmeList = styled.dl`
+& + & {
+  margin-top: 10px;
+}
+`
+
+const CategoryTitle = styled.dt`
+  font-size: ${({ theme }) => theme.rem.p14};
+  margin-bottom: 2px;
+  a {
+    display: block;
+    color: #fff;
+    padding: 5px;
+    border-radius: 5px;
+    transition: 0.2s;
+    &:hover {
+      background: ${({ theme }) => theme.color.err};
+    }
+  }
+`;
+
+const CategoryItem = styled.dd`
+  position: relative;
+  border-radius: 5px;
+  overflow: hidden;
+  margin-bottom: 2px;
+  a {
+    display: block;
+    color: #fff;
+    font-size: ${({ theme }) => theme.rem.p12};
+    word-break: keep-all;
+    padding: 5px 20px;
+    transition: 0.2s;
+    &:after {
+      content: '';
+      position: absolute;
+      display: block;
+      width: 4px;
+      height: 4px;
+      background: #fff;
+      border-radius: 4px;
+      left: 8px;
+      top: 10px;
+      transition: 0.2s;
+    }
+    &:hover {
+      background: #484848;
+      &:after {
+        background: ${({ theme }) => theme.color.err};
+      }
+    }
+  }
+`;
--- front/src/components/category/CategoryList.tsx
+++ front/src/components/category/CategoryList.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import Link from 'next/link';
-import customApi from '@utils/customApi';
-import { useQuery } from '@tanstack/react-query';
-import { GET_ALL_CATEGORY } from '@utils/queryKeys';
+import CategoryItems from './CategoryItems';
 
 export interface CategoryBackProps {
   id ?: number;
@@ -15,30 +12,12 @@ export  interface SubCategoryBackProps {
   categorySubTitle: string;
 }
 const CategoryList = () => {
-  const { getApi } = customApi('/category');
-  const { data, isLoading, isSuccess } = useQuery([GET_ALL_CATEGORY], getApi);
 
   return (
     <CategoryNav>
       <CategoryListBox>
         <h3>카테고리 목록</h3>
-        {data &&
-          data.map(({ categoryTitle, subCategories }: CategoryBackProps) => (
-            <>
-              <CategoryTitle>
-                <Link prefetch href={`/category/${categoryTitle}`}>
-                  {categoryTitle}
-                </Link>
-              </CategoryTitle>
-              {subCategories.map(({ categorySubTitle, id }: SubCategoryBackProps) => (
-                <CategoryItem>
-                  <Link prefetch href={`/category/${categoryTitle}/${categorySubTitle}`}>
-                    {categorySubTitle}
-                  </Link>
-                </CategoryItem>
-              ))}
-            </>
-          ))}
+        <CategoryItems/>
       </CategoryListBox>
     </CategoryNav>
   );
@@ -79,55 +58,5 @@ const CategoryListBox = styled.dl`
     padding: 0 0.313rem .5rem;
     margin-bottom: ${({ theme }) => theme.rem.p10};
     pointer-events: none;
-  }
-`;
-
-const CategoryTitle = styled.dt`
-  font-size: ${({ theme }) => theme.rem.p14};
-  a {
-    display: block;
-    color: #fff;
-    padding: 5px 5px 7px;
-    border-radius: 5px;
-    transition: 0.2s;
-    &:hover {
-      background: ${({ theme }) => theme.color.err};
-    }
-  }
-`;
-
-const CategoryItem = styled.dd`
-  position: relative;
-  border-radius: 5px;
-  overflow: hidden;
-  margin-bottom: 2px;
-  a {
-    display: block;
-    color: #fff;
-    font-size: ${({ theme }) => theme.rem.p12};
-    word-break: keep-all;
-    padding: 5px 20px;
-    transition: 0.2s;
-    &:after {
-      content: '';
-      position: absolute;
-      display: block;
-      width: 4px;
-      height: 4px;
-      background: #fff;
-      border-radius: 4px;
-      left: 8px;
-      top: 10px;
-      transition: 0.2s;
-    }
-    &:hover {
-      background: #484848;
-      &:after {
-        background: ${({ theme }) => theme.color.err};
-      }
-    }
-  }
-  & + ${CategoryTitle} {
-    margin-top: 10px;
   }
 `;
