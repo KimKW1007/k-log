af6fc7701bcade94d5af11afb5a4afb588c89195
KimKW1007 <face676934@gmail.com>
2023-07-15 21:30:08 +0900

    feat & test : HomeNewPoster 관련 layout 잡고 기능 테스트 중

--- front/src/components/home/HomeNewPoster.tsx
+++ front/src/components/home/HomeNewPoster.tsx
@@ -1,6 +1,9 @@
 import { OnlyAlignCenterFlex } from '@components/common/CommonFlex';
-import React from 'react'
+import React from 'react';
 import styled, { keyframes, css } from 'styled-components';
+import HomeNewPosterList from './HomeNewPosterList';
+import { FlexEmptyBox } from '@components/signup/signupForm';
+import ProjectSlide from '@components/projectBanner/ProjectSlide';
 
 const HomeNewPoster = () => {
   return (
@@ -12,28 +15,36 @@ const HomeNewPoster = () => {
           <p>개발공부(common)</p>
         </TitleDescBox>
       </TitleBox>
+      <FlexEmptyBox />
+      <HomeNewPosterList></HomeNewPosterList>
+      <ProjectSlide />
     </HomeNewPosterWrap>
-  )
-}
+  );
+};
 
-export default HomeNewPoster
-
+export default HomeNewPoster;
 
 const HomeNewPosterWrap = styled.div`
-  flex:1;
-`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
 
 const TitleBox = styled(OnlyAlignCenterFlex)`
-  display: inline-flex;
-  box-shadow : 15px 10px 5px 3px rgba(0,0,0,.2);
+  width: 380px;
+  box-shadow: 15px 10px 5px 3px rgba(0, 0, 0, 0.2);
+  border-right: 3px solid rgba(255, 255, 255, 0.1);
+  border-bottom: 3px solid rgba(255, 255, 255, 0.1);
+  border-left: 1px solid rgba(255, 255, 255, 0.1);
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
   padding: 20px 40px;
-`
+`;
 const Title = styled.h2`
   font-size: 40px;
   margin-right: 40px;
-`
+`;
 const TitleDescBox = styled.div`
-  p{
+  p {
     padding: 2px 0;
   }
-`
+`;
