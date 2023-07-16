8139bf1786077df8c95aee976f65f611bacc1ee4
KimKW1007 <face676934@gmail.com>
2023-07-16 21:21:16 +0900

    test & style : HomeNewPoster - 반응형 작업 중

--- front/src/components/home/HomeNewPosterList.tsx
+++ front/src/components/home/HomeNewPosterList.tsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled, { keyframes, css } from 'styled-components';
 import HomeNewPosterBigItem from './HomeNewPosterBigItem';
 import HomeNewPosterSmItem from './HomeNewPosterSmItem';
 import { OnlyAlignCenterFlex } from '@components/common/CommonFlex';
 
 const HomeNewPosterList = () => {
-  const bannerList = new Array(5).fill(undefined).map((val, idx) => idx);
+  const [testList , setTestList] = useState([
+    {id: 1},
+    {id: 2},
+    {id: 3},
+    {id: 4},
+    {id: 5},
+  ])
+  const posterList = new Array(5).fill(undefined).map((val, idx) => idx)
+  const wrapRef = useRef<HTMLDivElement>(null)
+  const smListRef = useRef<HTMLDivElement>(null)
 
+
+
+  
   return (
     <ListWrap>
       <ListContainer>
         <HomeNewPosterBigItem />
-        <NewPosterSmList>
-          {bannerList.map(ele=>(
-            <HomeNewPosterSmItem/>
+        <NewPosterSmList ref={smListRef}>
+          {testList.map((ele, idx)=>(
+            <HomeNewPosterSmItem key={`${Date.now() + 'salt' + idx}`} wrapRef={wrapRef} smListRef={smListRef} id={ele.id}/>
           ))}
         </NewPosterSmList>
+        
       </ListContainer>
+      <NewPosterSmWrapList ref={wrapRef}>
+        
+      </NewPosterSmWrapList>
     </ListWrap>
   )
 }
@@ -35,4 +51,10 @@ const ListContainer = styled.div`
 
 const NewPosterSmList = styled.div`
   display:flex;
+`
+
+const NewPosterSmWrapList = styled.div`
+  display:flex;
+  flex-wrap:wrap;
+  margin-top : 30px;
 `
