2d999eb7c6d9680337e26800318a004d4c4751d4
KimKW1007 <face676934@gmail.com>
2023-07-17 23:25:48 +0900

    style & chore : LoginSignupBox - props => isInSideMenu/  반응형 ㅜ가

--- front/src/components/header/LoginSignUpBox.tsx
+++ front/src/components/header/LoginSignUpBox.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 
-const LoginSignUpBox = () => {
+const LoginSignUpBox = ({isInSideMenu} : { isInSideMenu ?: boolean}) => {
   return (
     <>
-      <LoginText href={'/login'} title="로그인">
+      <LoginText href={'/login'} title="로그인" isInSideMenu={isInSideMenu}>
         로그인
       </LoginText>
-      <SignupText href={'/signup'} className="rightLine" title="회원가입">
+      <ShortLine></ShortLine>
+      <SignupText href={'/signup'} className="rightLine" title="회원가입" isInSideMenu={isInSideMenu}>
         회원가입
       </SignupText>
     </>
@@ -17,7 +18,7 @@ const LoginSignUpBox = () => {
 
 export default LoginSignUpBox;
 
-const LoginSignUpText = styled(Link)`
+const LoginSignUpText = styled(Link)<{isInSideMenu ?: boolean}>`
   padding: 0.5rem ${({ theme }) => theme.rem.p12};
   margin : 0 0.313em;
   transition:  .3s;
@@ -25,23 +26,35 @@ const LoginSignUpText = styled(Link)`
   font-size: 13px;
   color:#fff;
   background: #454545;
+  ${({isInSideMenu}) => isInSideMenu &&`
+    background: transparent !important;
+    font-size: 15px;
+    color:rgba(255,255,255,.5);
+    &:hover{
+      color:rgba(255,255,255,1);
+      background: transparent !important;
+    }
+  `}
 `;
 
 const LoginText = styled(LoginSignUpText)`
   &:hover{
     background: #408E91;
   }
+  
 `
 const SignupText = styled(LoginSignUpText)`
   background: #A0BFE0;
   &:hover{
     background: #4A55A2;
   }
+  
 `
 
-const ShortLine = styled.i`
+export const ShortLine = styled.i`
   display: inline-block;
   width: 1px;
   height: 1em;
-  background: black;
+  background: rgba(128,128,128,0.3);
+  margin: 0 10px;
 `;
