682e62354a74c99e507d85c46a17b3a472c39747
KimKW1007 <face676934@gmail.com>
2023-07-17 23:18:48 +0900

    feat : useScrollOverHeader

---
+++ front/src/hooks/useScrollOverHeader.ts
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+const useScrollOverHeader = () => {
+  const [isOverHeader, setIsOverHeader] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+       
+        if(window.scrollY >= 70){
+          setIsOverHeader(true)
+        }else{
+          setIsOverHeader(false)
+        }
+    });
+    return () => window.removeEventListener('scroll', () => {});
+  });
+
+  return {isOverHeader}
+}
+
+export default useScrollOverHeader
