03085d9b153558ccbad525c5fc13b3ba327f49ea
KimKW1007 <face676934@gmail.com>
2023-07-18 22:24:39 +0900

    feat : TitleItemList 임시 생성

---
+++ front/src/components/category/TitlePage/TitleItemList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled,{keyframes} from 'styled-components';
+
+const TitleItemList = () => {
+  return (
+    <div>TitleItemList</div>
+  )
+}
+
+export default TitleItemList
+
