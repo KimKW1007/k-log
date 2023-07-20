92917ba56c88879aedc30804ffdff063a24225e7
KimKW1007 <face676934@gmail.com>
2023-07-20 16:37:04 +0900

    chore : ImageApi - 불필요한 코드 제거

--- front/src/utils/ImageApi.ts
+++ front/src/utils/ImageApi.ts
@@ -16,36 +16,17 @@ const baseApi = () => {
   return axiosBase;
 };
 
-axiosBase.defaults.baseURL = 'http://localhost:5000/file';
 axiosBase.defaults.withCredentials = true;
 
 
-export default function imageApi<T = any>(url: string) {
+export default function imageApi<T = any>(url: string, isOriginServer ?: boolean) {
   const postApi = async (data: T) => {
-    const result = await baseApi().post(url, data, {
+    const result = await baseApi().post(`${isOriginServer ? `http://localhost:5000/file/` : 'http://localhost:8000/api/'}${url}`, data, {
       headers: {
         Authorization: 'Bearer ' + sessionStorage.getItem('jwtToken')
       }
     });
     return result.data;
   };
-  const getApi = async () => {
-    const result = await baseApi().get(url, {
-      headers: {
-        Authorization: 'Bearer ' + sessionStorage.getItem('jwtToken')
-      }
-    });
-    return result.data;
-  };
-
-  const deleteApi = async (data: T) => {
-    const result = await baseApi().delete(url, {
-      data,
-      headers: {
-        Authorization: 'Bearer ' + sessionStorage.getItem('jwtToken')
-      }
-    });
-    return result.data;
-  };
-  return { postApi, getApi, deleteApi };
+  return { postApi };
 }
