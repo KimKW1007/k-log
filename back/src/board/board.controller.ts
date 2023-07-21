dd9ccbdcbaab2ad95377e1b734a4a871b5778b3e
KimKW1007 <face676934@gmail.com>
2023-07-21 23:34:57 +0900

    feat & test(back) : board.cont/service - getLastBoardId

--- back/src/board/board.controller.ts
+++ back/src/board/board.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { BoardService } from './board.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(private boardService: BoardService) {}
+
+  @Get('/lastBoardId')
+  getBoardLastId(){
+    return this.boardService.getBoardLastId()
+  }
+
+
+}
--- back/src/board/board.service.ts
+++ back/src/board/board.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  constructor(
+    @InjectRepository(BoardRepository)
+    private boardRepository: BoardRepository,
+  ) {}
+
+
+  async getBoardLastId(){
+    const foundLastId =  await this.boardRepository.find({order : {id : 'desc'},take: 1})
+    if(!foundLastId[0]){
+      return 1
+    }
+    return foundLastId[0].id
+  }
+
+}
