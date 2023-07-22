09d7579d06b0efd2683cebebe8c97064ee712ad3
KimKW1007 <face676934@gmail.com>
2023-07-22 22:03:01 +0900

    feat(back) : board.service - createBoard

--- back/src/board/board.service.ts
+++ back/src/board/board.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { BoardRepository } from './board.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class BoardService {
@@ -9,13 +10,17 @@ export class BoardService {
     private boardRepository: BoardRepository,
   ) {}
 
+  async createBoard(body,file: Express.Multer.File, user : User){
+    return this.boardRepository.createBoard(body,file, user)
+  }
+
 
   async getBoardLastId(){
     const foundLastId =  await this.boardRepository.find({order : {id : 'desc'},take: 1})
     if(!foundLastId[0]){
       return 1
     }
-    return foundLastId[0].id
+    return foundLastId[0].id + 1
   }
 
 }
