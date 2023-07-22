4de9f5579a9ff29df4786889223b764d86ed97a5
KimKW1007 <face676934@gmail.com>
2023-07-22 22:11:44 +0900

    feat : BoardItem - CategoryPage 에서 공용으로 쓸 예정

---
+++ front/src/components/board/BoardItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components';
+
+const BoardItem = () => {
+  return (
+    <ItemWrap>BoardItem</ItemWrap>
+  )
+}
+
+export default BoardItem
+
+const ItemWrap = styled.div`
+
+`
