64063e1caf3ba3004188d28882c2fabc8f2c35ba
KimKW1007 <face676934@gmail.com>
2023-07-22 21:59:24 +0900

    feat(back) : board.cont - createBoard post API

--- back/src/board/board.controller.ts
+++ back/src/board/board.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 
 @Controller('board')
 export class BoardController {
@@ -9,6 +12,13 @@ export class BoardController {
   @Get('/lastBoardId')
   getBoardLastId(){
     return this.boardService.getBoardLastId()
+  }
+
+  @Post("/createBoard")
+  @UseInterceptors(FileInterceptor('boardImage'))
+  @UseGuards(AuthGuard())
+  createBoard(@Body() body, @UploadedFile() file: Express.Multer.File, @GetUser() user : User){
+    return this.boardService.createBoard(body, file, user)
   }
 
 
