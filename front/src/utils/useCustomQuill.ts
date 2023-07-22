89eef7380b6c961b8931797e90eeca7f996a4707
KimKW1007 <face676934@gmail.com>
2023-07-22 22:10:21 +0900

    feat : useCustomQuill - highlight.js 적용

--- front/src/utils/useCustomQuill.ts
+++ front/src/utils/useCustomQuill.ts
@@ -1,95 +1,81 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import imageApi from './ImageApi';
+import React, { useEffect, useMemo, useState } from 'react';
+import ifInImageApi from './ifInImageApi';
 import ReactQuill from 'react-quill';
 import customApi from './customApi';
 import { useQuery } from '@tanstack/react-query';
 import useIsMount from 'src/hooks/useIsMount';
 import { useRouter } from 'next/router';
+import hljs from 'highlight.js/lib/core';
 
-
-const useCustomQuill = (quillRef : React.RefObject<ReactQuill>, userId : string) => {
-  const {postApi} = imageApi('uploads');
-  const router = useRouter()
+const useCustomQuill = (quillRef: React.RefObject<ReactQuill>, userId: string) => {
+  const { postApi } = ifInImageApi('uploads');
+  const router = useRouter();
   const [currentSubTitle, setCurrentSubTitle] = useState('');
-  const { getApi } = customApi("/board/lastBoardId");
-  const {isMount} = useIsMount();
-  const {data : boardLastId} = useQuery(['GET_BOARD_LAST_ID'], getApi,{
+  const { getApi } = customApi('/board/lastBoardId');
+  const { isMount } = useIsMount();
+  const { data: boardLastId } = useQuery(['GET_BOARD_LAST_ID'], getApi, {
     enabled: !!isMount
-  })
+  });
 
-  useEffect(()=>{
-    if(router.query.subTitle){
-      setCurrentSubTitle(String(router.query.subTitle).split(' ').at(-1)!)
+  useEffect(() => {
+    if (router.query.subTitle) {
+      setCurrentSubTitle(String(router.query.subTitle).split(' ').at(-1)!);
     }
-  },[router.query.subTitle])
+  }, [router.query.subTitle]);
 
-
-  const handleImage =  () => {
-    const input = document.createElement("input");
-    input.setAttribute("type", "file");
-    input.setAttribute("accept", "image/*");
+  const handleImage = () => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
     input.click();
-    input.addEventListener("change", async () => {
-      if(input.files){
+    input.addEventListener('change', async () => {
+      if (input.files) {
         const file = input.files[0];
         const formData = new FormData();
-        formData.append("image", file);
-        formData.append("userId", userId);
-        formData.append("subTitle", currentSubTitle);
-        formData.append("boardId", String(boardLastId));
+        formData.append('image', file);
+        formData.append('userId', userId);
+        formData.append('subTitle', currentSubTitle);
+        formData.append('boardId', String(boardLastId));
         try {
           const res = await postApi(formData);
           const IMG_URL = res.url;
           const editor = quillRef.current?.getEditor();
           const range = editor?.getSelection();
-          editor?.insertEmbed(range?.index!, "image", `${IMG_URL}`);
+          editor?.insertEmbed(range?.index!, 'image', `${IMG_URL}`);
           editor?.setSelection(range?.index! + 1, 0);
         } catch (err) {
-          return "실패";
+          return '실패';
         }
       }
     });
-  }
-  const modules = useMemo(()=>{
-    return { 
+  };
+
+  const modules = useMemo(() => {
+    return {
+      syntax: {
+        highlight: (text: any) => hljs.highlightAuto(text).value
+      },
       blotFormatter: {},
-      toolbar:{
-        container:[
-          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-          [{ 'align': [] }],
-          [{ 'indent': '-1'}, { 'indent': '+1' }],  
-          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-          [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-          [{ 'color': [] }, { 'background': [] }],
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          [{ align: [] }],
+          [{ indent: '-1' }, { indent: '+1' }],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'],
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ color: [] }, { background: [] }],
           ['image', 'link'],
-          ['clean'] 
+          ['clean']
         ],
         handlers: {
-          image: handleImage,
+          image: handleImage
         }
-      },
-    }
-  },[currentSubTitle])
-  const formats = [
-    'header',
-    'font',
-    'size',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-    'color',
-    'background',
-    'align',
-  ]
+      }
+    };
+  }, [currentSubTitle]);
+  const formats = ['header', 'font', 'size', 'bold', 'italic', 'underline', 'strike', 'blockquote', 'list', 'bullet', 'indent', 'link', 'image', 'color', 'background', 'align', 'code-block'];
 
-  return { formats, modules, boardLastId }
-}
+  return { formats, modules, boardLastId };
+};
 
-export default useCustomQuill
+export default useCustomQuill;
