4b306d90f8d6be86608464217d11f360adab6dcf
KimKW1007 <face676934@gmail.com>
2023-07-24 20:38:25 +0900

    feat : category/[title]/[subCategroy]Page

--- front/pages/category/[title]/[subTitle]/index.tsx
+++ front/pages/category/[title]/[subTitle]/index.tsx
@@ -1,15 +1,32 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
-import styled,{keyframes} from 'styled-components';
 import type { NextPage } from 'next';
-import useIsMount from 'src/hooks/useIsMount';
+import customApi from '@utils/customApi';
+import { useQuery } from '@tanstack/react-query';
+import { GET_BOARDS } from '@utils/queryKeys';
+import BoardWrapComp from '@components/board/BoardWrapComp';
 
 const SubCategoryPage: NextPage = () => {
   const router = useRouter();
-  const {isMount} = useIsMount();
+  const [currentTitle, setCurrentTitle] = useState('');
+  const [currentSubTitle, setCurrentSubTitle] = useState('');
+  const {getApi} = customApi(`/board/subCategory/${currentSubTitle}`)
+  const { data : boardsList, isLoading } = useQuery([GET_BOARDS,currentSubTitle] , getApi, {
+    enabled: !!Boolean(currentTitle)
+  });
+ 
+  useEffect(()=>{
+    if(router.query){
+      const title = router.query.title;
+      const subTitle = router.query.subTitle;
+      setCurrentTitle(title + '/' + subTitle)
+      setCurrentSubTitle(String(subTitle))
+    }
+  },[router.query])
 
   return (
-    <div>{isMount && router.query.title + '/' + router.query.subTitle}</div>
+    <BoardWrapComp title={currentTitle} isLoading={isLoading} currentList={boardsList} />
+
   )
 }
 
