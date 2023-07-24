df43fb8f51a34894604eb22ea72f64c9fa2227ab
KimKW1007 <face676934@gmail.com>
2023-07-24 14:58:52 +0900

    chore(back) : file.mo - providers => add BoardRep, SubCateRep

--- back/src/file/file.module.ts
+++ back/src/file/file.module.ts
@@ -5,11 +5,13 @@ import { FileService } from './file.service';
 import { FileRepository } from './file.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { BoardRepository } from 'src/board/board.repository';
+import { SubCategoryRepository } from 'src/category/category.repository';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([FileRepository]),AuthModule],
   controllers: [FileController],
-  providers: [FileService, FileRepository],
+  providers: [FileService, FileRepository, BoardRepository, SubCategoryRepository],
 })
 export class FileModule {}
