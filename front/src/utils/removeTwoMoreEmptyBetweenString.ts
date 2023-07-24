f2b93b72cc4c38894001809f32c3c3aedd862d3d
KimKW1007 <face676934@gmail.com>
2023-07-24 20:47:09 +0900

    feat : removeEmptyBetweenString - 문자열 모든 공백 제거

--- front/src/utils/removeTwoMoreEmptyBetweenString.ts
+++ front/src/utils/removeTwoMoreEmptyBetweenString.ts
@@ -1,3 +1,6 @@
 export const removeTwoMoreEmptyBetweenString =(value : string)=>{
   return value.trim().replaceAll(/ +/g, " ")
 }
+export const removeEmptyBetweenString =(value : string)=>{
+  return value.trim().replaceAll(" ", "")
+}
