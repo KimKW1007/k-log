0996d6d92444104f3cf9487296d7a653298e8dc6
KimKW1007 <face676934@gmail.com>
2023-07-24 20:43:22 +0900

    feat & test : BoardItem api test

--- front/src/components/board/BoardItem.tsx
+++ front/src/components/board/BoardItem.tsx
@@ -1,72 +1,44 @@
 import { OnlyAlignCenterFlex } from '@components/common/CommonFlex';
 import React from 'react'
 import styled, { keyframes } from 'styled-components';
+import Image from "next/image";
+import defaultImage from '@assets/images/500_94.jpg';
+import useConvert from 'src/hooks/useConvert';
 
-const BoardItem = () => {
-  const test = `
-  <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테dsadasd스트</p>
-    <p>테스트</p> <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트dsadsa</p>
-    <p>테스트</p>
-    <p>테스트</p> <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p> <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테dsada스트</p>
-    <p>테스트</p>
-    <p>테스트</p> <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테스트</p>
-    <p>테dsadsas스트</p> <p>테스트</p>
-    <p>테스트</p>
-   
-    `
+
+
+const BoardItem = (board : any) => {
+  const {author, authorImage, boardTitle, contents, createdAt, thumbnail, subCategory : {categorySubTitle}  }  = board;
+  const  {reverseConvert} = useConvert();
   return (
     <ItemWrap>
       <ContentsBox>
         <ItemCategory>
           <span>
-            JavaScript
+            {categorySubTitle.split(" ").at(-1)}
           </span>
         </ItemCategory>
         <ItemTitleBox>
-          <p>어쩌구 저쩌구</p>
+          <p>{boardTitle}</p>
         </ItemTitleBox>
         <ItemDescBox>
-          {/* splice 해서 ... 찍던지 해야할듯 */}
-          {test.replace(/(<([^>]+)>)/gi, '')}
+          {reverseConvert(contents).replace(/(<([^>]+)>)/gi, '')}
         </ItemDescBox>
         <ItemDetailBox>
           <ItemAuthorBox>
             <AuthorImageBox>
-
+              <AuthorImage src={authorImage || defaultImage.src} alt={'프로필 이미지'} width={0} height={0} sizes='100vw' />
             </AuthorImageBox>
-            <span>관리자</span>
+            <span>{author}</span>
           </ItemAuthorBox>
           <CreatedDateBox>
-            2023.07.05
+            {createdAt.slice(0,10)}
           </CreatedDateBox>
         </ItemDetailBox>
       </ContentsBox>
-      <ImageBox></ImageBox>
+      <ImageBox>
+        <ImageBg src={thumbnail || defaultImage.src} />
+      </ImageBox>
     </ItemWrap>
   )
 }
@@ -74,13 +46,22 @@ const BoardItem = () => {
 export default BoardItem
 
 const ItemWrap = styled(OnlyAlignCenterFlex)`
-  padding: 40px 30px;
-  border : 1px solid #fff;
+  padding: 30px 30px 20px;
   column-gap: 30px;
+ 
+  @media(max-width:750px){
+    padding: 0;
+    flex-direction : column;
+  }
+
 `
 const ContentsBox =styled.div`
   flex: 1;
-
+  @media(max-width:750px){
+    width:100%;
+    padding: 20px;
+    order: 2;
+  }
 `
 const ItemCategory =styled.div`
 margin-bottom: 24px;
@@ -91,6 +72,9 @@ span{
   padding: 10px 15px;
   border-radius: 20px;
 }
+@media(max-width:750px){
+  margin-bottom : 16px;
+}
 `
 const ItemTitleBox =styled.div`
 font-size : 20px;
@@ -99,6 +83,9 @@ p{
   white-space : nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+}
+@media(max-width:750px){
+  margin-bottom : 16px;
 }
 `
 const ItemDescBox =styled.div`
@@ -114,15 +101,28 @@ const ItemDescBox =styled.div`
 `
 const ItemDetailBox =styled(OnlyAlignCenterFlex)`
   font-size: 14px;
+  @media(max-width:750px){
+    font-size: 1.8666vw;
+  }
+  @media(max-width:600px){
+    font-size: 12px;
+  }
 `
 const ItemAuthorBox =styled(OnlyAlignCenterFlex)`
   margin-right: 20px;
+ 
 `
 const AuthorImageBox= styled.div`
+  position:relative;
   width: 30px;
   height: 30px;
-  border : 1px solid #fff;
   margin-right: 10px;
+  border-radius: 5px;
+  overflow: hidden;
+  @media(max-width:750px){
+    width: 4vw;
+    height: 4vw;
+  }
 `
 
 const CreatedDateBox = styled.div`
@@ -130,13 +130,33 @@ const CreatedDateBox = styled.div`
 
 `
 
+const AuthorImage = styled(Image)`
+  width:100%;
+  height:auto;
+`
 
 
 
 
 const ImageBox = styled.div`
+  position: relative;
   width: 168px;
   height: 168px;
-  border : 1px solid #fff;
-  
+  overflow : hidden;
+  img{
+    width:100%;
+    height:100%;
+  }
+  @media(max-width:750px){
+    width: 100%;
+    height: 46.6666vw;
+    order: 1;
+    width: 100%;
+  }
+`
+
+const ImageBg =styled.div<{src : string;}>`
+  width:100%;
+  height:100%;
+  background: url(${({src}) => src}) no-repeat center center/cover;
 `
