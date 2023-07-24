3084eac4987af7254157d01d6df781e9036313ed
KimKW1007 <face676934@gmail.com>
2023-07-24 14:56:38 +0900

    chore(back) : board.mo - providers => add FileRep

--- back/src/board/board.module.ts
+++ back/src/board/board.module.ts
@@ -3,13 +3,14 @@ import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
-import { CategoryModule } from 'src/category/category.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { SubCategoryRepository } from 'src/category/category.repository';
+import { FileRepository } from 'src/file/file.repository';
+
 
 @Module({
-  imports:[TypeOrmModule.forFeature([BoardRepository]),CategoryModule, AuthModule],
+  imports:[TypeOrmModule.forFeature([BoardRepository]),AuthModule],
   controllers: [BoardController],
-  providers: [BoardService, BoardRepository, SubCategoryRepository]
+  providers: [BoardService, BoardRepository, SubCategoryRepository, FileRepository],
 })
 export class BoardModule {}
