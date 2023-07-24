96ee560ed2b797997e4af56f63fe8180b7abdb14
KimKW1007 <face676934@gmail.com>
2023-07-24 20:36:45 +0900

    feat(back) : board.rep - 생성 전 image파일 boardId = 작성중 => 생성 후 생성된 Id로 변경

--- back/src/board/board.repository.ts
+++ back/src/board/board.repository.ts
@@ -16,7 +16,8 @@ export class BoardRepository extends Repository<Board> {
     ) {
     super(Board, dataSource.createEntityManager());
   }
-  private readonly DATA_URL = 'http://localhost:8000/api/uploads';
+  private readonly DATA_IMAGE_URL = 'http://localhost:8000/api/uploads';
+  private readonly DATA_BOARD_ID_UPDATE = 'http://localhost:8000/api/createdBoard';
 
   async createBoard(body, file: Express.Multer.File, user: User) {
     const { boardTitle, contents, categorySubTitle, boardId } = body;
@@ -33,7 +34,9 @@ export class BoardRepository extends Repository<Board> {
           authorImage: authorImage !== null ? authorImage.imageUrl : '',
           subCategory,
         });
-        await this.save(newBoard);
+        await this.save(newBoard).then(async res=>{
+          const response = await axios.patch(`${this.DATA_BOARD_ID_UPDATE}/${user.id}`,{boardId : res.id})
+        });
       } catch (e) {
         console.log('오류 발생');
         throw new BadRequestException('board Create 중 오류 발생');
@@ -45,9 +48,9 @@ export class BoardRepository extends Repository<Board> {
       formData.append('image', imageBlob, file.originalname);
       formData.append('userId', String(user.id));
       formData.append('subTitle', categorySubTitle);
-      formData.append('boardId', boardId);
+      formData.append('boardId', '작성중');
       try {
-        const response = await axios.post(this.DATA_URL, formData, {
+        const response = await axios.post(this.DATA_IMAGE_URL, formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
@@ -61,7 +64,9 @@ export class BoardRepository extends Repository<Board> {
           authorImage: authorImage !== null ? authorImage.imageUrl : '',
           subCategory,
         });
-        await this.save(newBoard);
+        await this.save(newBoard).then(async res=>{
+          const response = await axios.patch(`${this.DATA_BOARD_ID_UPDATE}/${user.id}`,{boardId : res.id})
+        });
       } catch (e) {
         console.log('오류 발생');
         throw new BadRequestException('board Create 중 오류 발생');
