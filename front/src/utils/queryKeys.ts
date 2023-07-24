4617f93b95dbbd3c117e03e19addf607b9a2ade6
KimKW1007 <face676934@gmail.com>
2023-07-24 20:40:58 +0900

    chore : queryKeys - key 추가

--- front/src/utils/queryKeys.ts
+++ front/src/utils/queryKeys.ts
@@ -1,2 +1,13 @@
+/* 카테고리 */
 export const GET_ALL_CATEGORY = "GET_ALL_CATEGORY"
+
+/* 유저 */
 export const GET_USER = "GET_USER"
+export const GET_USER_MINI_PL = "GET_USER_MINI_PL"
+
+/* 쿠키 */
+export const GET_COOKIE = "GET_COOKIE"
+
+/* 게시물 */
+export const GET_BOARDS = "GET_BOARDS"
+export const GET_BOARD_LAST_ID = "GET_BOARD_LAST_ID"
