c1e531348869616a6d1919d4ed98973b5a763f72
KimKW1007 <face676934@gmail.com>
2023-07-25 23:38:44 +0900

    feat : AllCategoryPage - 분류 전체보기 페이지

---
+++ front/pages/category/index.tsx
@@ -0,0 +1,31 @@
+import { currentPagenation } from '@atoms/atoms';
+import BoardWrapComp from '@components/board/BoardWrapComp';
+import { useQuery } from '@tanstack/react-query';
+import customApi from '@utils/customApi';
+import { BOARD_ALL, GET_BOARDS } from '@utils/queryKeys';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil';
+import useIsMount from 'src/hooks/useIsMount';
+
+const AllCategoryPage = () => {
+  const router = useRouter();
+  const [currentPage, setCurrentPage] = useRecoilState(currentPagenation);
+  const {isMount} = useIsMount();
+  const {getApi} = customApi(`/board/category?page=${currentPage ?? 1}`)
+  const { data  , isLoading, refetch } = useQuery([GET_BOARDS, BOARD_ALL], getApi,{
+    enabled : !!isMount
+  });
+  useEffect(()=>{
+    setCurrentPage(1);
+  },[router])
+  useEffect(()=>{
+    refetch();
+  },[currentPage,isMount])
+
+  return (
+    <BoardWrapComp title={'분류 전체보기'} isLoading={isLoading} currentList={data?.boards} lastPage={data?.last_page}  />
+  )
+}
+
+export default AllCategoryPage
