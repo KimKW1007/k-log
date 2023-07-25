2b14c3404ccaa78735851a8462c1d2b0baf32e3f
KimKW1007 <face676934@gmail.com>
2023-07-25 23:31:05 +0900

    feat & refactor(back) : board.cont - feat => createLastBoardId/getAllBoard refactor => getBoardsFor(Category/SubCategory)

--- back/src/board/board.controller.ts
+++ back/src/board/board.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Query, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
@@ -9,10 +9,17 @@ import { User } from 'src/auth/user.entity';
 export class BoardController {
   constructor(private boardService: BoardService) {}
 
-  @Get('/lastBoardId')
-  getBoardLastId(){
-    return this.boardService.getBoardLastId()
+  @Get('/lastBoardId/:categorySubTitle')
+  @UseGuards(AuthGuard())
+  createLastBoardId(@Param('categorySubTitle') categorySubTitle : string,@GetUser() user : User){
+    return this.boardService.createLastBoardId(categorySubTitle, user)
   }
+
+/*   @Delete('/deleteTemporaryBoard/:categorySubTitle')
+  @UseGuards(AuthGuard())
+  deleteTemporaryBoard(@Param('categorySubTitle') categorySubTitle : string,@GetUser() user : User){
+    return this.boardService.deleteTemporaryBoard(categorySubTitle, user)
+  } */
 
   @Post("/createBoard")
   @UseInterceptors(FileInterceptor('boardImage'))
@@ -21,13 +28,20 @@ export class BoardController {
     return this.boardService.createBoard(body, file, user)
   }
 
+
+
+  @Get('/category')
+  getAllBoards(@Query("page" , ParseIntPipe) page : number = 1){
+    return this.boardService.getAllBoards(page)
+  }
+
   @Get('/category/:categoryTitle')
-  getBoardsForCategory(@Param("categoryTitle") categoryTitle : string){
-    return this.boardService.getBoardsForCategory(categoryTitle)
+  getBoardsForCategory(@Query("page" , ParseIntPipe) page : number = 1, @Param("categoryTitle") categoryTitle : string){
+    return this.boardService.getBoardsForCategory(categoryTitle, page)
   }
   @Get('/subCategory/:categorySubTitle')
-  getBoardsForSubCategory(@Param("categorySubTitle") categorySubTitle : string){
-    return this.boardService.getBoardsForSubCategory(categorySubTitle)
+  getBoardsForSubCategory(@Query("page" , ParseIntPipe) page : number = 1, @Param("categorySubTitle") categorySubTitle : string){
+    return this.boardService.getBoardsForSubCategory(categorySubTitle, page)
   }
 
 }
