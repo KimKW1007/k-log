9b50742cf80495cdec83ab7d390fa6a1099aa063
KimKW1007 <face676934@gmail.com>
2023-07-26 22:04:29 +0900

    test : [:id](BoardPage) - api 연결 테스트

--- front/pages/[id].tsx
+++ front/pages/[id].tsx
@@ -1,11 +1,22 @@
+import BoardDetail from '@components/board/BoardDetail'
+import { GetServerSideProps, NextPage } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { CategoryPageProps } from './category/[title]/[subTitle]'
 
-const BoardPage = () => {
-  const router = useRouter()
+const BoardPage : NextPage = ({id} : CategoryPageProps) => {
   return (
-    <div>{router.query && router.query.id}</div>
+    <div>
+      <BoardDetail id={id}/>
+    </div>
   )
 }
 
+export const  getServerSideProps: GetServerSideProps = async ( context  )=>{
+  const {query} = context;
+  const {id} = query;
+  return {
+    props : { id }
+  }
+}
 export default BoardPage
