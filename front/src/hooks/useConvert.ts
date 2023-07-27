a3b0e7af4b9da4b53813ed5f01678e28cd7f4de9
KimKW1007 <face676934@gmail.com>
2023-07-27 23:57:19 +0900

    feat : useConvert - decodeHTMLEntities 추가

--- front/src/hooks/useConvert.ts
+++ front/src/hooks/useConvert.ts
@@ -1,19 +1,32 @@
-import React from 'react'
+import React from 'react';
 /**
  * @convertContent : HTML -> DB 저장 할 때
  * @reverseConvert : DB -> HTML 로 변환
  */
 const useConvert = () => {
-  const convertContent = (contents: string)=>{
-    const convertValue = contents.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
-    return convertValue
-  }
-  const reverseConvert = (contents: string)=>{
-    const reverseConvertValue = contents.replaceAll("&gt;", ">").replaceAll("&lt;", "<");
-    return reverseConvertValue
-  }
+  const convertContent = (contents: string) => {
+    const convertValue = contents.replaceAll('<', '&lt;').replaceAll('>', '&gt;');
+    return convertValue;
+  };
+  const reverseConvert = (contents: string) => {
+    const reverseConvertValue = contents.replaceAll('&gt;', '>').replaceAll('&lt;', '<');
+    return reverseConvertValue;
+  };
+  const decodeHTMLEntities = (str: any) => {
+    if (str === undefined && str === null && str === '') {
+      return '';
+    }
+    return str
+      .replace(/&amp;/g, '&')
+      .replace(/&nbsp;/g, ' ')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>')
+      .replace(/&quot;/g, '"')
+      .replace(/&#039;/g, "'")
+      .replace(/&#39;/g, "'");
+  };
 
-  return {convertContent, reverseConvert}
-}
+  return { convertContent, reverseConvert, decodeHTMLEntities };
+};
 
-export default useConvert
+export default useConvert;
