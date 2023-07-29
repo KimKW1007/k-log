0ebda6cf3935c6e91e21d147a9be949cc55510bc
KimKW1007 <face676934@gmail.com>
2023-07-30 00:04:19 +0900

    fix : LoginForm - userData getApi isNecessaryToken추가로 인한 코드변경

--- front/src/components/login/LoginForm.tsx
+++ front/src/components/login/LoginForm.tsx
@@ -37,7 +37,7 @@ const LoginForm = () => {
     },
     async onSuccess(data) {
       sessionStorage.setItem('jwtToken', data.accessToken);
-      const userData = await getApi();
+      const userData = await getApi(true);
       setUser(userData);
       router.back();
     }
