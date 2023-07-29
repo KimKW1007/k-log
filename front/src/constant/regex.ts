6b04670162ff67700aedce995a143b6087ebd2fb
KimKW1007 <face676934@gmail.com>
2023-07-29 21:01:40 +0900

    fix : regex - ACCOUNT_ID_REGEX => 조건문 변경

--- front/src/constant/regex.ts
+++ front/src/constant/regex.ts
@@ -1,4 +1,4 @@
 export const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-export const ACCOUNT_ID_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9]).{4,24}$/;
+export const ACCOUNT_ID_REGEX = /^(?=.*[0-9]+)[a-zA-Z][a-zA-Z0-9]{4,24}$/;
 export const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$/;;
 export const NAME_REGEX = /^[가-힣a-zA-Z]{2,4}$/;
