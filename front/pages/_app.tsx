e4bbe34ad4d54c178841bacfbf8547444910a190
KimKW1007 <face676934@gmail.com>
2023-07-29 23:33:14 +0900

    feat : _app - CustomSeo 추가

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -6,16 +6,19 @@ import Layout from '@components/layout/Layout';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
+import CustomSeo from '@utils/Seo/CustomSeo';
 
 const queryClient = new QueryClient({});
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const { seoData } = pageProps;
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
           <Layout>
+            <CustomSeo seoData={seoData} />
             <Component {...pageProps} />
           </Layout>
         </ThemeProvider>
