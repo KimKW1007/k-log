f0ebf18c0ba2e4713a95b038467a9a004db1bf62
KimKW1007 <face676934@gmail.com>
2023-07-30 13:13:24 +0900

    refactor & feat & test : EditCategoryList - dnd test 중

--- front/src/components/accountEdit/EditCategory/EditCategoryList.tsx
+++ front/src/components/accountEdit/EditCategory/EditCategoryList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import styled,{keyframes, css} from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import customApi from '@utils/customApi';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { GET_ALL_CATEGORY } from '@utils/queryKeys';
 import AddCategoryBtn from './AddCategoryBtn';
 import { FormProvider, useFieldArray, useForm } from 'react-hook-form';
@@ -9,126 +9,163 @@ import EditCategoryItemBox from './EditCategoryItemBox';
 import { OnlyJustifyCenterFlex } from '@components/common/CommonFlex';
 import { removeTwoMoreEmptyBetweenString } from '@utils/removeTwoMoreEmptyBetweenString';
 import { CategoryBackProps } from '@components/category/CategoryList';
-
-
+import { DragDropContext, Draggable, DropResult, Droppable } from '@hello-pangea/dnd';
 
 const EditCategoryList = () => {
   // mutate - putApi 관련
   const [isError, setIsError] = useState(false);
   const [isChangeValue, setIsChangeValue] = useState(false);
   const queryClient = useQueryClient();
-  const {putApi} = customApi("category/updateCategories");
-  const {mutate, isLoading} = useMutation(putApi,{
-    onError(error : any) {
+  const { putApi } = customApi('category/updateCategories');
+  const { mutate, isLoading } = useMutation(putApi, {
+    onError(error: any) {
       setIsError(true);
-      alert(error.response.data.message)
+      alert(error.response.data.message);
     },
     onSuccess(data) {
-      console.log({data})
-      setIsChangeValue(false)
-    },
-  })
+      console.log({ data });
+      setIsChangeValue(false);
+    }
+  });
 
   // getAPI 관련
   const { getApi } = customApi('/category');
-  const { data } = useQuery([GET_ALL_CATEGORY], ()=> getApi());
-
+  const { data } = useQuery([GET_ALL_CATEGORY], () => getApi());
 
   // useForm
   const methods = useForm({
     mode: 'all',
-    defaultValues : useMemo(()=>{
-      return {category: data}
-    },[data])
+    defaultValues: useMemo(() => {
+      return { category: data };
+    }, [data])
   });
 
-  const { control, handleSubmit, reset, watch, formState:{isDirty}  } = methods;
-  const { append, remove, fields } = useFieldArray({
+  const {
     control,
-    name: "category",
+    handleSubmit,
+    reset,
+    watch,
+    formState: { isDirty, dirtyFields }
+  } = methods;
+  const { append, remove, fields, move } = useFieldArray<{category : any }>({
+    control,
+    name: 'category'
   });
 
   const onSubmit = (data: any) => {
-    if(isError) return;
+    if (isError) return;
     const categoryData = data.category;
-    const filteredCategory = categoryData.filter((x :CategoryBackProps) => x.categoryTitle === '');
-    const filteredSubCategory = categoryData.map((x :CategoryBackProps) => x.subCategories.filter(v => v.categorySubTitle === '')).flat();
-    if(filteredCategory.length >= 1 ){
-      alert("상위 카테고리 중 비어있는 카테고리가 있습니다")
-      return 
+    const filteredCategory = categoryData.filter((x: CategoryBackProps) => x.categoryTitle === '');
+    const filteredSubCategory = categoryData.map((x: CategoryBackProps) => x.subCategories.filter((v) => v.categorySubTitle === '')).flat();
+    if (filteredCategory.length >= 1) {
+      alert('상위 카테고리 중 비어있는 카테고리가 있습니다');
+      return;
     }
-    if(filteredSubCategory.length >= 1){
-      alert("하위 카테고리 중 비어있는 카테고리가 있습니다")
-      return 
+    if (filteredSubCategory.length >= 1) {
+      alert('하위 카테고리 중 비어있는 카테고리가 있습니다');
+      return;
     }
     const copiedCategoryData = [...categoryData];
-    const result = copiedCategoryData.map((cateTitle:CategoryBackProps)=>{
-      cateTitle = { ...cateTitle, categoryTitle: removeTwoMoreEmptyBetweenString(cateTitle.categoryTitle) }
-      const cateSubTitle = cateTitle.subCategories.map((cateSubTitle: { categorySubTitle: string; }) =>{
-        return {...cateSubTitle, categorySubTitle : removeTwoMoreEmptyBetweenString(cateSubTitle.categorySubTitle)}
-      })
-      cateTitle = {...cateTitle, subCategories : cateSubTitle}
-      return cateTitle
-    })
-    mutate(result)
-  }
+    const result = copiedCategoryData.map((cateTitle: CategoryBackProps, idx : number) => {
+      cateTitle = { ...cateTitle, categoryTitle: removeTwoMoreEmptyBetweenString(cateTitle.categoryTitle), dndNumber : idx + 1 };
+      const cateSubTitle = cateTitle.subCategories.map((cateSubTitle: { categorySubTitle: string }) => {
+        return { ...cateSubTitle, categorySubTitle: removeTwoMoreEmptyBetweenString(cateSubTitle.categorySubTitle) };
+      });
+      cateTitle = { ...cateTitle, subCategories: cateSubTitle };
+      return cateTitle;
+    });
+    mutate(result);
+  };
 
-
-  useEffect(()=>{
-    if(isDirty){
+  useEffect(() => {
+    if (isDirty) {
       setIsChangeValue(true);
-    }else{
+    } else {
       setIsChangeValue(false);
     }
-  },[isDirty])
-  
+  }, [isDirty]);
+
   // input에 변화가 일어나면 error false
   useEffect(() => {
-    const subscription = watch((value, { name, type }) =>{
-      if(type === 'change'){
+    const subscription = watch((value, { name, type }) => {
+      if (type === 'change') {
         setIsError(false);
         setIsChangeValue(true);
       }
-    })
-    return () => subscription.unsubscribe()
-  }, [watch])
-
-
+    });
+    return () => subscription.unsubscribe();
+  }, [watch]);
 
   // 기본값
-  useEffect(()=>{
-    reset({category : data})
+  useEffect(() => {
+    reset({ category: data });
     queryClient.invalidateQueries([GET_ALL_CATEGORY]);
-  },[data])
+  }, [data]);
+  useEffect(() => {
+    dirtyFields.category?.map(({dndNumber} :  CategoryBackProps)=>{
+      if(dndNumber){
+        setIsChangeValue(true);
+      } else {
+        setIsChangeValue(false);
+      }
+    })
+  }, [dirtyFields]);
+
+  const onDragEnd =(result : DropResult)=>{
+    console.log({result})
+    move(result.source.index, result.destination?.index!);
+  }
+  
   
 
   return (
     <CategoryNav>
-      <CategoryListBox >
+      <CategoryListBox>
         <h3>카테고리 목록</h3>
         <FormProvider {...methods}>
           <Form onSubmit={handleSubmit(onSubmit)}>
-            {fields.map((category, index)=>(
-              <EditCategoryItemBox
-                categoryIndex = {index}
-                key={category.id}
-                remove={remove}
-              />
-            ))}
-            <AddCategoryBtn onClick={()=>{append({categoryTitle:"",subCategories:[]})}}></AddCategoryBtn>
-                <EditBtnBox>
-            <EditBtn  disabled={!isChangeValue} isError={isError} isChangeValue={isChangeValue}>
-              {isLoading ? "수정 중" :isChangeValue ? "수정하기" : "수정완료"}
-            </EditBtn>
-          </EditBtnBox>
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="category">
+                {(magic) => (
+                  <div ref={magic.innerRef} {...magic.droppableProps}>
+                    {fields.map((category, index) => {
+                      return (
+                        <Draggable key={String(category.id)} draggableId={String(category.id)} index={index}>
+                          {(magic) => (
+                            <div
+                            ref={magic.innerRef}
+                            {...magic.draggableProps}
+                            {...magic.dragHandleProps}
+                            >
+                              <EditCategoryItemBox categoryIndex={index} key={category.id} remove={remove} />
+                            </div>
+                          )}
+                        </Draggable>
+                      )
+                    })}
+                  {magic.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+
+            <AddCategoryBtn
+              onClick={() => {
+                append({ dndNumber: fields.length, categoryTitle: '', subCategories: [] });
+              }}></AddCategoryBtn>
+            <EditBtnBox>
+              <EditBtn disabled={!isChangeValue} isError={isError} isChangeValue={isChangeValue}>
+                {isLoading ? '수정 중' : isChangeValue ? '수정하기' : '수정완료'}
+              </EditBtn>
+            </EditBtnBox>
           </Form>
         </FormProvider>
       </CategoryListBox>
     </CategoryNav>
-  )
-}
+  );
+};
 
-export default EditCategoryList
+export default EditCategoryList;
 
 const OnErrorShakeAni = keyframes`
   0% {
@@ -146,26 +183,27 @@ const OnErrorShakeAni = keyframes`
   100% {
       transform: rotate(0px);
     }
-`
+`;
 
 const EditBtnBox = styled(OnlyJustifyCenterFlex)`
   width: 100%;
   margin-top: ${({ theme }) => theme.rem.p20};
 `;
-export const EditBtn = styled.button<{ isChangeValue: boolean; isError ?:boolean; }>`
+export const EditBtn = styled.button<{ isChangeValue: boolean; isError?: boolean }>`
   width: 114px;
   padding: 10px 30px;
   border-radius: 6px;
   color: #fff;
   background: #454545;
-  transition: .3s;
+  transition: 0.3s;
 
   &:disabled {
     background: #888;
     pointer-events: none;
   }
-  ${({ isError,isChangeValue }) =>
-    !isError&&isChangeValue &&
+  ${({ isError, isChangeValue }) =>
+    !isError &&
+    isChangeValue &&
     `
     background: #62afb2;
     &:hover{
@@ -173,38 +211,32 @@ export const EditBtn = styled.button<{ isChangeValue: boolean; isError ?:boolean
     }
   `}
   ${({ isError, theme }) =>
-    isError && css`
-    background: ${theme.color.err};
-    animation : ${OnErrorShakeAni} .3s both;
-    cursor: not-allowed;
-  `}
+    isError &&
+    css`
+      background: ${theme.color.err};
+      animation: ${OnErrorShakeAni} 0.3s both;
+      cursor: not-allowed;
+    `}
 `;
-const Form = styled.form`
-
-`
-
-
+const Form = styled.form``;
 
 const CategoryNav = styled.nav`
-position: relative;
-z-index : 3;
-  width:100%;
-margin-top: 2px;
-padding: 20px 30px ;
+  position: relative;
+  z-index: 3;
+  width: 100%;
+  margin-top: 2px;
+  padding: 20px 30px;
 `;
-
-
 
 const CategoryListBox = styled.div`
-display: flex;
-flex-direction: column;
-color: #232323;
-h3 {
-  font-size: ${({ theme }) => theme.rem.p14};
-  border-bottom: 1px solid #858585;
-  padding: 0 5px 8px;
-  margin-bottom: 10px;
-  pointer-events: none;
-}
+  display: flex;
+  flex-direction: column;
+  color: #232323;
+  h3 {
+    font-size: ${({ theme }) => theme.rem.p14};
+    border-bottom: 1px solid #858585;
+    padding: 0 5px 8px;
+    margin-bottom: 10px;
+    pointer-events: none;
+  }
 `;
-
