ec55c0b97a2b63631710adf24e4474748cab51d6
KimKW1007 <face676934@gmail.com>
2023-07-30 13:07:28 +0900

    feat(back) : user.repo - getCurrentHashedRefreshToken

--- back/src/auth/user.repository.ts
+++ back/src/auth/user.repository.ts
@@ -8,11 +8,15 @@ import { AuthRegistrationDto } from './dto/auth-registration.dto';
 import { AuthCheckEmailDto } from './dto/auth-checkEmail.dto';
 import { AuthChangeThingsDto } from './dto/auth-changeThings.dto';
 import { BoardRepository } from 'src/board/board.repository';
+import * as config from "config"
+
+
+const jwtConfig = config.get("jwt")
 
 @Injectable()
 export class UserRepository extends Repository<User> {
     constructor(private dataSource: DataSource,
-      private boardRepository: BoardRepository
+      private boardRepository: BoardRepository,
       ) {   
     super(User, dataSource.createEntityManager());
   }
@@ -103,4 +107,40 @@ export class UserRepository extends Repository<User> {
 
     return {message : "success"}
   }
+
+  async getCurrentHashedRefreshToken(refreshToken: string) {
+    // 토큰 값을 그대로 저장하기 보단, 암호화를 거쳐 데이터베이스에 저장한다. 
+    // bcrypt는 단방향 해시 함수이므로 암호화된 값으로 원래 문자열을 유추할 수 없다. 
+    const salt = await bcrypt.genSalt();
+    const currentRefreshToken = await bcrypt.hash(refreshToken, salt);
+    return currentRefreshToken;
+  }
+
+  async getCurrentRefreshTokenExp(): Promise<Date> {
+    const currentDate = new Date();
+  	// Date 형식으로 데이터베이스에 저장하기 위해 문자열을 숫자 타입으로 변환 (paresInt) 
+    const currentRefreshTokenExp = new Date(currentDate.getTime() + parseInt(jwtConfig.JWT_REFRESH_TOKEN_EXPIRATION_TIME));
+    return currentRefreshTokenExp;
+  }
+
+  async getUserIfRefreshTokenMatches(refreshToken: string, userId: number): Promise<User> {
+    const user: User = await this.findOneBy({id : userId});
+	
+	// user에 currentRefreshToken이 없다면 null을 반환 (즉, 토큰 값이 null일 경우)
+    if (!user.currentRefreshToken) {
+      return null;
+    }
+	
+    // 유저 테이블 내에 정의된 암호화된 refresh_token값과 요청 시 body에 담아준 refresh_token값 비교
+    const isRefreshTokenMatching = await bcrypt.compare(
+      refreshToken,
+      user.currentRefreshToken
+    );
+
+	// 만약 isRefreshTokenMatching이 true라면 user 객체를 반환
+    if (isRefreshTokenMatching) {
+      return user;
+    } 
+  }
+
 }
