fc3ee0856229294b02cb7ec4eea345419fd14edf
KimKW1007 <face676934@gmail.com>
2023-08-01 00:44:01 +0900

    style & feat : AccountEdit - 반응형 구현 및  mobTab 추가

--- front/src/components/accountEdit/AccountEdit.tsx
+++ front/src/components/accountEdit/AccountEdit.tsx
@@ -1,28 +1,44 @@
-import React, { useState } from 'react'
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react'
+import styled, { css } from 'styled-components';
 import AccountEditInputsBox from './AccountEditInputsBox';
-import { accountEditTabList } from '@utils/accountEditTabList';
-import { OnlyAlignCenterFlex } from '@components/common/CommonFlex';
+import { accountEditTabList, accountEditMobTabList } from '@utils/accountEditTabList';
+import { AllCenterFlex, OnlyAlignCenterFlex } from '@components/common/CommonFlex';
 
-const AccountEdit = () => {
-  const [currentTab, setCurrentTab] = useState("개인정보변경");
+interface AccountEditProps{
+  isForward : boolean;
+  isDisappear : boolean;
+  currentTab : string;
+  setCurrentTab: React.Dispatch<React.SetStateAction<string>>
+}
+
+
+const AccountEdit = ({isForward, isDisappear, currentTab, setCurrentTab} :AccountEditProps) => {
   const onClickTab =(title : string) => ()=>{
     setCurrentTab(title)
   }
+
+
   return (
     <>
-    <TabBox>
+    {isDisappear || <TabBox>
       {accountEditTabList.map((title, idx)=>(
-        <Tab onClick={onClickTab(title)} key={idx + 'salt' + title} isCurrent={title === currentTab}>
+        <Tab onClick={onClickTab(title)} key={idx + 'salt' + title} isCurrent={title === currentTab} isForward={isForward}>
           {title}
         </Tab>
       ))}
-    </TabBox>
+    </TabBox>}
     <EditAreaBox>
       <EditTitleBox>
         <h2>{currentTab}</h2>
       </EditTitleBox>
       <EditBox>
+        {isDisappear && <MobTabBox>
+          {accountEditMobTabList.map((title, idx)=>(
+            <MobTap onClick={onClickTab(title)} key={idx + 'saltMob' + title} isCurrent={title === currentTab}>
+              {title}
+            </MobTap>
+          ))}
+        </MobTabBox>}
         <EditInnerBox>
           <AccountEditInputsBox currentTab={currentTab}></AccountEditInputsBox>
         </EditInnerBox>
@@ -34,15 +50,43 @@ const AccountEdit = () => {
 
 export default AccountEdit
 
-const TabBox = styled(OnlyAlignCenterFlex)`
-  position: absolute;
-  z-index: 2;
-  left: 100%;
-  top: 30px;
-  flex-direction: column;
+const MobTap = styled.button<{isCurrent : boolean}>`
+  border-bottom : 2px solid #999999a1;
+  color: #999999a1;
+  margin : 0 10px;
+  padding: 10px 30px;
+  background :transparent;
+  font-size: 17px;
+  font-weight: 700;
+  transition: .2s;
+  
+  ${({isCurrent}) => isCurrent ? css`
+    border-bottom : 2px solid #232323;
+    color: #232323;
+  ` : css`
+  &:hover{
+    border-bottom : 2px solid #767676;
+    color: #767676;
+  }
+  `}
+  @media(max-width: 660px){
+    font-size: 15px;
+  }
 `
 
-const Tab = styled.button<{isCurrent : boolean;}>`
+const MobTabBox= styled(AllCenterFlex)`
+  display:none;
+  flex-wrap : wrap;
+  width:100%;
+  margin-bottom: 30px;
+  row-gap : 10px;
+  @media(max-width: 980px){
+    display:flex;
+  }
+`
+
+const Tab = styled.button<{isCurrent : boolean; isForward :boolean;}>`
+  position: relative;
   font-size: 16px;
   padding: 50px 25px 50px 35px;
   font-weight: 900;
@@ -51,17 +95,42 @@ const Tab = styled.button<{isCurrent : boolean;}>`
   background: #656565;
   border-bottom: 1px solid rgba(0,0,0,.2);
   box-shadow: 5px 12px 5px 3px rgba(0,0,0,.2);
-  transition: 0.2s;
   writing-mode: vertical-lr;
   transform :translateX(-20px);
+  transition: 0.2s;
   ${({isCurrent}) => isCurrent && `
     color: #232323;
     background: #fff;
-    padding: 50px 25px 50px 25px;
     transform :translateX(-10px);
   `}
-  `
-
+  @media(max-width: 1500px){
+    pointer-events: none;
+    &:after{
+      content: '';
+      position :absolute;
+      z-index :4;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height:100%;
+      background : rgba(0,0,0,.4);
+      border-radius: 0 20px 20px 0;
+    }
+    ${({isForward}) => isForward &&`
+      pointer-events: auto;
+      &:after{
+        display:none;
+      }
+    `}
+  }
+`
+  const TabBox = styled(OnlyAlignCenterFlex)`
+  position: absolute;
+  z-index: 2;
+  left: 100%;
+  top: 30px;
+  flex-direction: column;
+`
 const EditAreaBox = styled.div`
   position:relative;
   z-index: 3;
@@ -69,26 +138,53 @@ const EditAreaBox = styled.div`
   background: #fff;
   border-radius: 30px;
   overflow:hidden;
-  padding: ${({theme}) => theme.rem.p60} ${({theme}) => theme.rem.p20};
+  padding: 60px 20px;
+  @media(max-width: 980px){
+    padding: 60px 0;
+  }
+  @media(max-width: 980px){
+    border-radius: 0;
+    border : 0;
+    background: transparent;
+  }
 `
 
 export const EditTitleBox = styled.div`
-  margin-bottom: ${({theme}) => theme.rem.p50};
-  padding: 0 ${({theme}) => theme.rem.p50};
+  margin-bottom: 50px;
+  padding: 0 50px;
   h2{
-    font-size :${({theme}) => theme.rem.p34};
+    font-size :34px;
+  }
+  @media(max-width: 980px){
+    padding: 0 5vw;
+    margin-bottom: 80px;
+    h2{
+      font-size :30px;
+    }
   }
 `
 
 const EditBox = styled.div`
   width:100%;
-  padding: ${({theme}) => theme.rem.p60};
+  padding: 60px;
+  @media(max-width: 980px){
+    padding: 0 60px 60px;
+  }
+  @media(max-width: 900px){
+    padding: 0 2.2222vw 60px;
+  }
 `
 const EditInnerBox = styled.div`
   width:100%;
   border : 1px solid #DDE6ED;
   border-radius: 30px;
   overflow:hidden;
-  padding: ${({theme}) => theme.rem.p30};
+  padding: 30px;
+  @media(max-width: 660px){
+    padding:  0;
+    border-radius: 0;
+    border : 0;
+    border-top : 1px solid #DDE6ED;
+  }
 `
 
