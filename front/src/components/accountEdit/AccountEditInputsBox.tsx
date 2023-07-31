d43cdd4a8e724d45fd45336cbc2908c985c510f5
KimKW1007 <face676934@gmail.com>
2023-08-01 00:45:20 +0900

    feat & style : AccountEditInputsBox - 모바일 시 카테고리 탭 추가 및 반응형 구현

--- front/src/components/accountEdit/AccountEditInputsBox.tsx
+++ front/src/components/accountEdit/AccountEditInputsBox.tsx
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 import AccountEditBox from './AccountEditBox';
 import { accountEditInputList } from '@utils/userInfoEdit';
 import ChangePassword from './ChangePassword';
+import EditSideBar from './EditSidbar/EditSideBarBox';
 
-const AccountEditInputsBox = ({currentTab} : {currentTab :string}) => {
+const AccountEditInputsBox = ({currentTab } : {currentTab :string;}) => {
   return (
     <InputsWrap>
       <InputsBox>
-      { currentTab === '개인정보변경' && accountEditInputList.map(({ name, title }, idx) => (
-          <AccountEditBox title={title} key={idx + 'salt' + title} name={name}></AccountEditBox>
+        {currentTab === '카테고리' && <EditSideBar></EditSideBar>}
+        {currentTab === '개인정보변경' && accountEditInputList.map(({ name, title }, idx) => (
+          <AccountEditBox title={title} key={idx + 'salt' + title} name={name} ></AccountEditBox>
         ))}
         {currentTab === '비밀번호변경' && <ChangePassword></ChangePassword>}
       </InputsBox>
@@ -24,6 +26,13 @@ const InputsWrap = styled.div`
 `;
 
 const InputsBox = styled.div`
+  position:relative;
   width: 100%;
-  padding: 40px 40px;
+  padding: 40px;
+  @media(max-width: 1300px){
+    padding: 40px 20px;
+  }
+  @media(max-width: 722px){
+    padding: 40px 0;
+  }
 `;
