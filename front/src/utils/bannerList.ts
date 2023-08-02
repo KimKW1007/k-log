af20ca32c3ac03dba39b990965e5ae411a82ec8a
KimKW1007 <face676934@gmail.com>
2023-08-02 23:40:16 +0900

    test : bannerList - 이미지 고려 중

--- front/src/utils/bannerList.ts
+++ front/src/utils/bannerList.ts
@@ -1,18 +1,21 @@
 import winterBg from '@assets/images/winterBg.jpg';
 import fallBg from '@assets/images/fallBg.jpg';
 import springBg from '@assets/images/springBg.jpg';
+import bg1 from '@assets/images/bg1.svg';
+import bg2 from '@assets/images/bg2.svg';
+import bg3 from '@assets/images/bg3.svg';
 import { ListTypes } from './mapList';
 
 //  배경은 3개만
 
-const banner1 = winterBg.src;
-const banner2 = fallBg.src;
-const banner3 = springBg.src;
+const banner1 = bg1.src;
+const banner2 = bg2.src;
+const banner3 = bg3.src;
 
 export const banner: ListTypes = {
-  banner1: winterBg.src,
-  banner2: fallBg.src,
-  banner3: springBg.src
+  banner1: bg1.src,
+  banner2: bg2.src,
+  banner3: bg3.src
 };
 
 export { banner1, banner2, banner3 };
