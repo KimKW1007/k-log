c389204c84502cb18ddcc8a910f75b99e1e908cd
KimKW1007 <face676934@gmail.com>
2023-08-03 23:53:10 +0900

    feat: HomeNewPosterList - getNewBoards api 연경

--- front/src/components/home/HomeNewPosterList.tsx
+++ front/src/components/home/HomeNewPosterList.tsx
@@ -3,28 +3,35 @@ import styled, { keyframes, css } from 'styled-components';
 import HomeNewPosterBigItem from './HomeNewPosterBigItem';
 import HomeNewPosterSmItem from './HomeNewPosterSmItem';
 import { OnlyAlignCenterFlex } from '@components/common/CommonFlex';
+import customApi from '@utils/customApi';
+import { useQuery } from '@tanstack/react-query';
+import { EmojiDizzyFill } from '@styled-icons/bootstrap/EmojiDizzyFill';
+import { EmptyIconBox } from '@components/board/BoardWrapComp';
+import BoardItem from '@components/board/BoardItem';
 
 const HomeNewPosterList = () => {
-  const [testList , setTestList] = useState([
-    {id: 1},
-    {id: 2},
-    {id: 3},
-  ])
-  const [wrapList, setWrapList] = useState([])
-
-
+  const { getApi } = customApi('/board/getNewBoards')
+  const { data } = useQuery(["GET_NEW_BOARDS"], ()=> getApi())
 
 
   
   return (
     <ListWrap>
       <ListContainer>
-        <HomeNewPosterBigItem />
-        <NewPosterSmList >
-          {testList.map((ele, idx)=>(
-            <HomeNewPosterSmItem key={`${ele.id +'salt' + idx}`} />
-          ))}
-        </NewPosterSmList>
+        {data || 
+          <EmptyBoardBox>
+            <EmojiDizzyFill />
+            <p>준비된 게시물이 없어요..</p>
+          </EmptyBoardBox>
+        }
+        {data && <>
+          <HomeNewPosterBigItem {...data?.[0]} />
+          <NewPosterSmList >
+            {data.slice(1).map((board: { id: string; }, idx: string)=>(
+              <BoardItem key={`${board.id +'salt' + idx}`} {...board} />
+            ))}
+          </NewPosterSmList>
+        </>}
       </ListContainer>
     </ListWrap>
   )
@@ -32,35 +39,35 @@ const HomeNewPosterList = () => {
 
 export default HomeNewPosterList
 
+
+const EmptyBoardBox = styled(EmptyIconBox)`
+  min-height:500px;
+`
+
 const ListWrap = styled.div`
   width:100%;
   margin-bottom: 30px;
 `
 
 const ListContainer = styled.div`
+  position: relative;
   width:100%;
   display:flex;
-  @media (max-width: 1020px){
+  @media (max-width: 1200px){
     display:block;
   }
 `
 
 const NewPosterSmList = styled.div`
-  width:530px;
+  width:50%;
   display:flex;
   flex-direction : column;
   @media (max-width: 1280px){
     width: 41.4062vw;
   }
-  @media (max-width: 1020px){
+  @media (max-width: 1200px){
     width: 100%;
     margin-top: 30px;
   }
 `
 
-const NewPosterSmWrapList = styled.div`
-  display:flex;
-  flex-wrap:wrap;
-  margin-top : 30px;
-
-`
