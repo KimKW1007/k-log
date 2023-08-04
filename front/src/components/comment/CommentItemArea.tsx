5bcfbd8e9beeaa5f4c978e6c3fd8a6df6030b4f0
KimKW1007 <face676934@gmail.com>
2023-08-05 00:50:16 +0900

    test : CommentItemArea - api test

--- front/src/components/comment/CommentItemArea.tsx
+++ front/src/components/comment/CommentItemArea.tsx
@@ -4,19 +4,25 @@ import CommentItem from './CommentItem';
 import CommentForm from './CommentForm';
 import customApi from '@utils/customApi';
 import { useRouter } from 'next/router';
+import { useQuery } from '@tanstack/react-query';
 
-const CommentItemArea = () => {
+const CommentItemArea = ({id} : {id :string}) => {
   const router= useRouter();
   const [currentBoardId, setCurrentBoardId] = useState(-1);
   const [replyIndex, setReplyIndex] = useState(-1);
-  const {getApi} = customApi('/comment/getTargetBoardComment')
-
-
+  const {getApi} = customApi(`/comment/getTargetBoardComment/${id}`)
+  const {data} = useQuery(["GET_COMMENTS", id], ()=>getApi())
+  console.log("{data}",{data})
   return (
-    <CommentItemBox>
-      <CommentItem setReplyIndex={setReplyIndex}/>
-      <CommentForm isReply isWriter/>
-    </CommentItemBox>
+    <>
+      {data && data.map((comment: any, index : number) => (
+      <CommentItemBox>
+        <CommentItem setReplyIndex={setReplyIndex} comment={comment}/>
+        <CommentForm isReply isWriter id={id}/>
+      </CommentItemBox>
+      ))
+    }
+    </>
   )
 }
 
