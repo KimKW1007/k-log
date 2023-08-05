bb9e762fd45989b4cf75ef31c15ac58a71c7d78d
KimKW1007 <face676934@gmail.com>
2023-08-05 16:29:55 +0900

    feat: CommentList - 불필요한 컴포넌트 제거 후 합침 댓글 리스트 뽑아 나열함

--- front/src/components/comment/CommentList.tsx
+++ front/src/components/comment/CommentList.tsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled, { keyframes, css } from 'styled-components';
-import CommentItemArea from './CommentItemArea';
+import CommentItem from './CommentItem';
+import CommentForm from './CommentForm';
+import customApi from '@utils/customApi';
+import { useRouter } from 'next/router';
+import { useQuery } from '@tanstack/react-query';
+import { GET_COMMENTS } from '@utils/queryKeys';
 
 const CommentList = ({id} : {id :string}) => {
+  const router= useRouter();
+  const [replyIndex, setReplyIndex] = useState(-1);
+  const {getApi} = customApi(`/comment/getTargetBoardComment/${id}`)
+  const {data} = useQuery([GET_COMMENTS, id], ()=>getApi())
+  const {comments, writerId} = data ?? {};
+  console.log({comments})
   return (
     <CommentListBox>
-      <CommentItemArea id={id} />
+      {comments?.length > 0 || <EmptyCommentBox>
+        <p>아직 댓글이 없어요!</p>
+        <p>첫 댓글을 작성해주세요!</p>
+        </EmptyCommentBox>}
+      {comments?.length > 0 && comments.map((comment: any, index : number) => (
+        <CommentItemBox key={comment.id}>
+          <CommentItem writerId={writerId} ownerId={comment.authorId} setReplyIndex={setReplyIndex} comment={comment} boardId={id}/>
+          {replyIndex === comment.id && <CommentForm isReply id={id} commentId={comment.id} setReplyIndex={setReplyIndex}/>}
+          {comment.replies.length > 0 && comment.replies.map((reply : any) =>(
+            <CommentItem isReply writerId={writerId} ownerId={comment.authorId} setReplyIndex={setReplyIndex} comment={reply} boardId={id}/>
+          ))}
+        </CommentItemBox>
+      ))}
     </CommentListBox>
   )
 }
 
 export default CommentList
 
+const CommentItemBox=  styled.div`
+  padding: 50px 0 ;
+  border-bottom : 4px dotted #a5a5a5a1;
+`
+
 const CommentListBox = styled.div`
   width:100%;
+`
+const EmptyCommentBox = styled.div`
+  width:100%;
+  text-align:center;
+  padding: 50px;
+  
 `
