507b245e268716a8c71b28f2ee7fbc14cce3a471
KimKW1007 <face676934@gmail.com>
2023-08-05 16:15:16 +0900

    chore(back) : reply.repo - entity 변경으로 인한 수정

--- back/src/comment/reply.repository.ts
+++ back/src/comment/reply.repository.ts
@@ -6,32 +6,42 @@ import { BoardRepository } from 'src/board/board.repository';
 import { User } from 'src/auth/user.entity';
 import { Reply } from './reply.entity';
 import { CommentRepository } from './comment.repository';
+import { FileRepository } from 'src/file/file.repository';
 
 @Injectable()
 export class ReplyRepository extends Repository<Reply> {
-  constructor(private dataSource: DataSource, private commentRepository : CommentRepository ) {
+  constructor(private dataSource: DataSource, private commentRepository : CommentRepository, private fileRepository : FileRepository ) {
     super(Reply, dataSource.createEntityManager());
   }
 
   async createReplyComment(commentId : number, commentCreateDto : CommentCreateDto, user: User){
-    const {userEmail, userName, id: userId} = user;
-    const {comment : replyComment, isSecret} = commentCreateDto;
+    const {userEmail, userName, id} = user;
+    const {comment, isSecret} = commentCreateDto;
+    const authorImage = await this.fileRepository.findOneBy({ user: { id: user.id } });
     const foundComment = await this.commentRepository.findOneBy({id : commentId});
     if(!foundComment){
       throw new  BadRequestException('해당 댓글을 찾을 수 없습니다.')
     }
     const newReplyComment = this.create({
-      userEmail,
-      userName,
-      userId,
-      replyComment,
+      authorEmail: userEmail,
+      authorName : userName,
+      authorId : id,
+      comment,
+      authorImage : authorImage?.imageUrl ??  '',
       isSecret : isSecret ?? "false",
-      comment : foundComment
+      connectedComment : foundComment
     })
     await this.save(newReplyComment);
   }
   async deleteReplyComment(replyId : number, user: User){
-    const comment = await this.findOneBy({id : replyId, comment : {board : { subCategory : { category : { user : { id : user.id}}}}}})
+    let comment;
+    const checkAuthor = await this.findOne({where :{id : replyId} , relations : { connectedComment : {board : {subCategory : {category : {user :true}}}}}})
+    const writerId = checkAuthor.connectedComment.board.subCategory.category.user.id
+    if(user.id === 1 || comment.authorId === writerId){
+      comment = await this.findOneBy({id : replyId})
+    }else{
+      comment = await this.findOneBy({id : replyId, authorId : user.id})
+    }
     if(!comment)throw new BadRequestException('삭제할 댓글이 없습니다.')
     await this.delete({id : replyId})
   }
