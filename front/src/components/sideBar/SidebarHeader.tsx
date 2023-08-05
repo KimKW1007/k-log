63a9d1d7e69b38c8a9d2c550f25be73f9bdd91d1
KimKW1007 <face676934@gmail.com>
2023-08-05 16:32:15 +0900

    chore : SidebarHeader - queryKey 겹침으로 인한 queryKey 추가 => ADMIN

--- front/src/components/sideBar/SidebarHeader.tsx
+++ front/src/components/sideBar/SidebarHeader.tsx
@@ -5,11 +5,11 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 import defaultImage from '@assets/images/500_94.jpg';
-import { GET_USER_MINI_PL } from '@utils/queryKeys';
+import { ADMIN, GET_USER_MINI_PL } from '@utils/queryKeys';
 
 const SidebarHeader = () => {
-  const { getApi } = customApi('/file/getUserPl');
-  const { data } = useQuery([GET_USER_MINI_PL], () => getApi());
+  const { getApi } = customApi('/file/getAdminPl');
+  const { data } = useQuery([GET_USER_MINI_PL,ADMIN], () => getApi());
 
   const [imageUrl, setImageUrl] = useState('');
   const [description, setDescription] = useState('');
