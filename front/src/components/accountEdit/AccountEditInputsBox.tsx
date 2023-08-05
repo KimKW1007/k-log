97ba080a30538d22a6dc0def9eb522cae8ea8f58
KimKW1007 <face676934@gmail.com>
2023-08-05 16:20:49 +0900

    feat : AccountEditInputsBox - Withdraw 추가

--- front/src/components/accountEdit/AccountEditInputsBox.tsx
+++ front/src/components/accountEdit/AccountEditInputsBox.tsx
@@ -4,16 +4,22 @@ import AccountEditBox from './AccountEditBox';
 import { accountEditInputList } from '@utils/userInfoEdit';
 import ChangePassword from './ChangePassword';
 import EditSideBar from './EditSidbar/EditSideBarBox';
+import { useRecoilState } from 'recoil';
+import { userInfomation } from '@atoms/atoms';
+import Withdraw from './Withdraw';
 
 const AccountEditInputsBox = ({currentTab } : {currentTab :string;}) => {
+  const [currentUser, setCurrentUser] = useRecoilState(userInfomation);
+
   return (
     <InputsWrap>
-      <InputsBox>
-        {currentTab === '카테고리' && <EditSideBar></EditSideBar>}
+      <InputsBox> 
+        {currentTab === '카테고리' && <EditSideBar isAdmin={currentUser?.id === 1}></EditSideBar>}
         {currentTab === '개인정보변경' && accountEditInputList.map(({ name, title }, idx) => (
           <AccountEditBox title={title} key={idx + 'salt' + title} name={name} ></AccountEditBox>
         ))}
         {currentTab === '비밀번호변경' && <ChangePassword></ChangePassword>}
+        {currentTab === '회원탈퇴' && <Withdraw></Withdraw>}
       </InputsBox>
     </InputsWrap>
   );
@@ -29,10 +35,7 @@ const InputsBox = styled.div`
   position:relative;
   width: 100%;
   padding: 40px;
-  @media(max-width: 1300px){
-    padding: 40px 20px;
-  }
-  @media(max-width: 722px){
+  @media(max-width: 1080px){
     padding: 40px 0;
   }
 `;
