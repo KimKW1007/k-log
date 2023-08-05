b85765ee6ad87f0487c2e1228573a1ff9552bc5f
KimKW1007 <face676934@gmail.com>
2023-08-05 16:33:04 +0900

    chore : accountEditTabList/accountEditMobTabList -  회원탈퇴 추가

--- front/src/utils/accountEditTabList.ts
+++ front/src/utils/accountEditTabList.ts
@@ -1,4 +1,4 @@
 
 
-export const accountEditTabList = [ "개인정보변경", "비밀번호변경"]
-export const accountEditMobTabList = [ "카테고리", "개인정보변경", "비밀번호변경",]
+export const accountEditTabList = [ "개인정보변경", "비밀번호변경","회원탈퇴"]
+export const accountEditMobTabList = [ "카테고리", "개인정보변경", "비밀번호변경","회원탈퇴"]
