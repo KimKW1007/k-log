a110eda5ba3a9c22440641f1599b012b11a260d3
KimKW1007 <face676934@gmail.com>
2023-08-05 16:33:22 +0900

    chore : queryKeys - USERS/ADMIN 추가

--- front/src/utils/queryKeys.ts
+++ front/src/utils/queryKeys.ts
@@ -4,12 +4,19 @@ export const GET_ALL_CATEGORY = "GET_ALL_CATEGORY"
 /* 유저 */
 export const GET_USER = "GET_USER"
 export const GET_USER_MINI_PL = "GET_USER_MINI_PL"
+export const USERS = "USERS"
+export const ADMIN = "ADMIN"
 
 /* 쿠키 */
 export const GET_COOKIE = "GET_COOKIE"
 
 /* 게시물 */
+export const GET_NEW_BOARDS = "GET_NEW_BOARDS"
 export const GET_BOARDS = "GET_BOARDS"
 export const GET_BOARD = "GET_BOARD"
 export const GET_BOARD_LAST_ID = "GET_BOARD_LAST_ID"
 export const BOARD_ALL = "BOARD_ALL"
+
+/* 댓글 */
+export const GET_COMMENTS = "GET_COMMENTS"
+
