8bac1c75067e1f0244e3912d98f158bdeb6fe693
KimKW1007 <face676934@gmail.com>
2023-08-05 16:32:42 +0900

    feat : useCheckLogin - 두번이상 쓰여서 만듬

---
+++ front/src/hooks/useCheckLogin.ts
@@ -0,0 +1,22 @@
+import { userInfomation } from '@atoms/atoms';
+import { useRouter } from 'next/router';
+import React from 'react'
+import { useRecoilState } from 'recoil';
+
+const useCheckLogin = () => {
+  const router = useRouter();
+  const [currentUser, setCurrentUser] = useRecoilState(userInfomation);
+  const checkLogin = (callback : ()=> void)=>{
+    if(!currentUser?.id){
+      if(confirm("로그인 후 이용가능합니다.\n로그인 페이지로 이동하시겠습니까?")){
+        router.push("/login")
+      }
+      return
+    } else {
+      callback(); // Execute the callback if user is logged in
+    }
+  }
+  return {checkLogin}
+}
+
+export default useCheckLogin
