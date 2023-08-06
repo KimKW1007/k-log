e5c39a4043c34323dcccc75e78597cd3594baf43
KimKW1007 <face676934@gmail.com>
2023-08-06 22:13:28 +0900

    refactor : HomeNewPosterList - list가 없을 시 emptyBoardBox

--- front/src/components/home/HomeNewPosterList.tsx
+++ front/src/components/home/HomeNewPosterList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import styled, { keyframes, css } from 'styled-components';
 import HomeNewPosterBigItem from './HomeNewPosterBigItem';
 import HomeNewPosterSmItem from './HomeNewPosterSmItem';
@@ -11,64 +11,72 @@ import BoardItem from '@components/board/BoardItem';
 import { GET_NEW_BOARDS } from '@utils/queryKeys';
 
 const HomeNewPosterList = () => {
-  const { getApi } = customApi('/board/getNewBoards')
-  const { data } = useQuery([GET_NEW_BOARDS], ()=> getApi())
+  const { getApi } = customApi('/board/getNewBoards');
+  const { data } = useQuery([GET_NEW_BOARDS], () => getApi());
 
-
-  
   return (
     <ListWrap>
       <ListContainer>
-        {data?.length > 0 || 
+        {data?.length > 0 || (
           <EmptyBoardBox>
             <EmojiDizzyFill />
             <p>준비된 게시물이 없어요..</p>
           </EmptyBoardBox>
-        }
-        {data?.length > 0 && <>
-          <HomeNewPosterBigItem {...data?.[0]} />
-          <NewPosterSmList >
-            {data.slice(1).map((board: { id: string; }, idx: string)=>(
-              <BoardItem key={`${board.id +'salt' + idx}`} {...board} />
-            ))}
-          </NewPosterSmList>
-        </>}
+        )}
+        {data?.length > 0 && (
+          <>
+            <HomeNewPosterBigItem {...data?.[0]} />
+            <NewPosterSmList>
+              {data?.length > 1 ? (
+                <>
+                  {data.slice(1).map((board: { id: string }, idx: string) => (
+                    <BoardItem key={`${board.id + 'salt' + idx}`} {...board} />
+                  ))}
+                </>
+              ) : (
+                <EmptyBoardBox>
+                  <EmojiDizzyFill />
+                  <p>더 만들고 있어요 잠시만요!</p>
+                </EmptyBoardBox>
+              )}
+            </NewPosterSmList>
+          </>
+        )}
       </ListContainer>
     </ListWrap>
-  )
-}
+  );
+};
 
-export default HomeNewPosterList
-
+export default HomeNewPosterList;
 
 const EmptyBoardBox = styled(EmptyIconBox)`
-  min-height:500px;
-`
+  min-height: 500px;
+  margin: auto;
+`;
 
 const ListWrap = styled.div`
-  width:100%;
+  width: 100%;
   margin-bottom: 30px;
-`
+`;
 
 const ListContainer = styled.div`
   position: relative;
-  width:100%;
-  display:flex;
-  @media (max-width: 1200px){
-    display:block;
+  width: 100%;
+  display: flex;
+  @media (max-width: 1200px) {
+    display: block;
   }
-`
+`;
 
 const NewPosterSmList = styled.div`
-  width:50%;
-  display:flex;
-  flex-direction : column;
-  @media (max-width: 1280px){
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 1280px) {
     width: 41.4062vw;
   }
-  @media (max-width: 1200px){
+  @media (max-width: 1200px) {
     width: 100%;
     margin-top: 30px;
   }
-`
-
+`;
