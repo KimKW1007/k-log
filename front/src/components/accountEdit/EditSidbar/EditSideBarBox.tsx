8ebd812db93bcdfb3e8e63a0a5506fdeab470cfa
KimKW1007 <face676934@gmail.com>
2023-08-08 00:25:24 +0900

    chor e: EditSideBarBox - 불필요한 코드제거

--- front/src/components/accountEdit/EditSidbar/EditSideBarBox.tsx
+++ front/src/components/accountEdit/EditSidbar/EditSideBarBox.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import profileImg from "@images/500_94.jpg"
-import Image from 'next/image';
 import { OnlyAlignCenterFlex } from '@components/common/CommonFlex';
-import SearchBox from '../../sideBar/SearchBox';
 import EditSidebarHeader from './EditSidebarHeader';
 import EditCategoryList from '@components/accountEdit/EditCategory/EditCategoryList';
 import LockIcon from '../LockIcon';
