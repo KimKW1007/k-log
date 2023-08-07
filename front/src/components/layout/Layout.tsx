93eb34fdeb428fa43727a271e8e8cfe6b8ba83b9
KimKW1007 <face676934@gmail.com>
2023-08-08 00:31:35 +0900

    feat: Layout - search를 위한 데이터 및 SearchModal 추가 추후 조건문 추가예정

--- front/src/components/layout/Layout.tsx
+++ front/src/components/layout/Layout.tsx
@@ -6,13 +6,23 @@ import { useRouter } from 'next/router';
 import { Header } from '@components/header/Header';
 import IconLinkListBox from '@components/common/IconLinkListBox';
 import Footer from '@components/footer/Footer';
+import customApi from '@utils/customApi';
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilState } from 'recoil';
+import { searchModalState } from '@atoms/atoms';
+import SeachModal from 'src/search/SeachModal';
 
 const Layout = ({ children }: ChildrenProps) => {
+  const [isOpenSeachModal, setIsOpenSearchModal] = useRecoilState(searchModalState);
   const [isAboutAuth, setIsAboutAuth] = useState(false);
   const router = useRouter();
   const routerPathCheck = (value: string) => {
     return router.pathname.includes(value);
   };
+
+  const {getApi} = customApi('/board/getSearchBoard');
+  const {data : searchData} = useQuery(["GET_SEARCH_DATA"], ()=> getApi());
+  console.log({searchData})
   const popupPage = routerPathCheck('/identity/find') || routerPathCheck('/accountEdit/changeEmail')
   useEffect(() => {
     if (routerPathCheck('login') || routerPathCheck('signup') || popupPage) {
@@ -27,6 +37,7 @@ const Layout = ({ children }: ChildrenProps) => {
       {isAboutAuth ? <IconLinkListBox popupPage={popupPage} /> : <Header />}
       {children}
       <Footer />
+      {isOpenSeachModal && <SeachModal boards={searchData} onClose={()=>setIsOpenSearchModal(false)}/>}
     </Root>
   );
 };
