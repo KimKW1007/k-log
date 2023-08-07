07e93bd5f81147432247e1c26aa9feed7a5c0905
KimKW1007 <face676934@gmail.com>
2023-08-08 00:30:30 +0900

    style & chore  : HomeSidebar - css로 인한 구조변경

--- front/src/components/home/HomeSidebar.tsx
+++ front/src/components/home/HomeSidebar.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import styled, { keyframes, css } from 'styled-components';
-import SearchBox from '../sideBar/SearchBox';
+import SearchBox from '../../search/SearchBox';
 import { OnlyAlignCenterFlex } from '@components/common/CommonFlex';
 import SidebarHeader from '@components/sideBar/SidebarHeader';
 import SidebarCategoryBox from '@components/sideBar/SidebarCategoryBox';
@@ -8,31 +8,26 @@ import SidebarCategoryBox from '@components/sideBar/SidebarCategoryBox';
 const HomeSidebar = () => {
   return (
     <SidebarBox>
-      <SidebarTopBox>
-        <SidebarHeader/>
-        <SearchBox />
-      </SidebarTopBox>
-      <SidebarCategoryBox/>
+      <SidebarHeader />
+      <SearchBox />
+      <SidebarCategoryBox />
     </SidebarBox>
-  )
-}
+  );
+};
 
-export default HomeSidebar
+export default HomeSidebar;
 const SidebarBox = styled(OnlyAlignCenterFlex)`
   position: sticky;
   top: 100px;
   right: 0;
-  flex-direction : column;
+  flex-direction: column;
   width: 360px;
   max-height: 800px;
-  border : 1px solid #DDE6ED;
+  border: 1px solid #dde6ed;
   border-radius: 30px;
-  overflow:hidden;
-  padding: ${({theme}) => theme.rem.p20} 0;
+  overflow: hidden;
+  padding: ${({ theme }) => theme.rem.p20} 0;
   background: #343434;
   row-gap: 40px;
   flex-shrink: 0;
 `;
-const SidebarTopBox = styled.div`
-  padding: 0 50px;
-`
