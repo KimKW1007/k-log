98eda061771d73f70cd3d4b8f8620cebe1d29d54
KimKW1007 <face676934@gmail.com>
2023-08-08 00:35:50 +0900

    refactor : codeBlockStyler - 생성 및 수정 할때 line이 없어서 hljs 가 적용됐지만 read는 line을 나눠서 hljs 적용 되게 수정

--- front/src/utils/codeBlockStyler.ts
+++ front/src/utils/codeBlockStyler.ts
@@ -1,14 +1,33 @@
 import React from 'react'
 import useConvert from 'src/hooks/useConvert';
+import "highlight.js/styles/atom-one-dark.css";
 
 const codeBlockStyler = (ref : React.RefObject<HTMLDivElement>) => {
   if(ref.current){
     const codeWrappers = ref.current.querySelectorAll('pre[class^=ql-syntax]');
     if (codeWrappers.length > 0) {
+      let isHljsString = 'off';
       codeWrappers.forEach((codeWrapper) => {
         if (codeWrapper) {
           const codes = codeWrapper.innerHTML.split('\n').slice(0,-1);
-          const processedCodes = codes.reduce((prevCodes: string, curCode: string) => prevCodes + `<div class="line">${curCode}</div>`, '');
+          const processedCodes = codes.reduce((prevCodes: string, curCode: string) => {
+            let code = curCode;
+            if(curCode.endsWith('<span class="hljs-string">`')) {
+              if(isHljsString==='off'){
+                isHljsString = 'on'
+              }
+            }else if(curCode.includes('`</span>')){
+              if(isHljsString==='on'){
+                isHljsString = 'off'
+                code = `<span class="hljs-string">${curCode.split('</span>')[0]}</span>;`
+              }
+            }else{
+              if(isHljsString === 'on'){
+                code = `<span class="hljs-string">${curCode}</span>`
+              }
+            };
+            return prevCodes + `<div class="line">${code}</div>`
+          }, '');
           const codeBody = `<div class="code-body">${processedCodes}</div>`;
 
           const codeHeader = `
