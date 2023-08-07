1392874e8e4ec4f2fe083243c91c84dd3007c7b0
KimKW1007 <face676934@gmail.com>
2023-08-08 00:28:36 +0900

    chore : SideMenu - prettier 적용

--- front/src/components/header/SideMenu.tsx
+++ front/src/components/header/SideMenu.tsx
@@ -1,7 +1,7 @@
-import SearchBox from '@components/sideBar/SearchBox';
+import SearchBox from 'src/search/SearchBox';
 import SidebarCategoryBox from '@components/sideBar/SidebarCategoryBox';
 import SidebarHeader from '@components/sideBar/SidebarHeader';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import styled, { keyframes, css } from 'styled-components';
 import HamburgerMenuBtn from './HamburgerMenuBtn';
 import { AllCenterFlex, OnlyAlignCenterFlex, OnlyJustifyCenterFlex } from '@components/common/CommonFlex';
@@ -13,62 +13,57 @@ import HeaderUserMenu from './HeaderUserMenu';
 import LoginSignUpBox from './LoginSignUpBox';
 import { OnDeviceTraining } from '@styled-icons/material-rounded';
 
-
 const SideMenu = () => {
-  const {handleClickMenu , isOpen, isActive} = useHandleSideMenu();
+  const { handleClickMenu, isOpen, isActive } = useHandleSideMenu();
   const [userInfo, setUserInfo] = useRecoilState(userInfomation);
   const { isMount } = useIsMount();
- 
+
   return (
     <>
-    <HamburgerMenuBtn handleClickMenu={handleClickMenu} isOpen={isOpen} />
-    {isActive && <SideMenuWrap>
-      <SideMenuDim  isOpen={isOpen} onClick={handleClickMenu}/>
-      <SideMenuArea className='customScroll'>
-        <SideMenuContainer>
-          <LogoBox>
-            K : Log
-          </LogoBox>
-          <RelevantToTheUser>
-            {isMount && userInfo ? <HeaderUserMenu isInSideMenu /> : <LoginSignUpBox isInSideMenu />}
-          </RelevantToTheUser>
-          <SidebarBox>
-            <SidebarHeader/>
-            <SearchBox />
-            <SidebarCategoryBox isMenu/>
-          </SidebarBox>
-          <SideMenuFooter>
-            COPYRIGHT &copy; 2023 Kim Kyenong Won.
-            <br />
-            ALL RIGHTS RESERVED.
-          </SideMenuFooter>
-        </SideMenuContainer>
-      </SideMenuArea>
-    </SideMenuWrap>}
+      <HamburgerMenuBtn handleClickMenu={handleClickMenu} isOpen={isOpen} />
+      {isActive && (
+        <SideMenuWrap>
+          <SideMenuDim isOpen={isOpen} onClick={handleClickMenu} />
+          <SideMenuArea className="customScroll">
+            <SideMenuContainer>
+              <LogoBox>K : Log</LogoBox>
+              <RelevantToTheUser>{isMount && userInfo ? <HeaderUserMenu isInSideMenu /> : <LoginSignUpBox isInSideMenu />}</RelevantToTheUser>
+              <SidebarBox>
+                <SidebarHeader />
+                <SearchBox />
+                <SidebarCategoryBox isMenu />
+              </SidebarBox>
+              <SideMenuFooter>
+                COPYRIGHT &copy; 2023 Kim Kyenong Won.
+                <br />
+                ALL RIGHTS RESERVED.
+              </SideMenuFooter>
+            </SideMenuContainer>
+          </SideMenuArea>
+        </SideMenuWrap>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default SideMenu
+export default SideMenu;
 
-
-const SideMenuWrap =styled.div`
+const SideMenuWrap = styled.div`
   position: fixed;
-  left:0;
-  top:0;
+  left: 0;
+  top: 0;
   width: 100vw;
-  height:100vh;
+  height: 100vh;
   z-index: 2000;
-
-`
-const SideMenuDim =styled.div<{ isOpen: boolean }>`
+`;
+const SideMenuDim = styled.div<{ isOpen: boolean }>`
   position: absolute;
-  width:100%;
-  height:100%;
-  left:0;
-  top:0;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
   z-index: 10;
-  background: rgba(0,0,0,.2);
+  background: rgba(0, 0, 0, 0.2);
   opacity: ${({ isOpen }) => (isOpen ? `1` : `0`)};
   transition: opacity 0.6s;
   ${({ isOpen }) =>
@@ -78,8 +73,8 @@ const SideMenuDim =styled.div<{ isOpen: boolean }>`
         transform: translate(0, 0);
       }
     `}
-`
-const SideMenuArea =styled.div`
+`;
+const SideMenuArea = styled.div`
   position: absolute;
   right: 10px;
   bottom: 0;
@@ -93,39 +88,37 @@ const SideMenuArea =styled.div`
   transition: transform 0.6s;
   overflow: hidden;
   color: #fff;
-  background:#23262d;
+  background: #23262d;
   padding: 30px;
-  overflow-y : scroll;
+  overflow-y: scroll;
   &::-webkit-scrollbar-track {
     margin-top: 100px;
   }
-`
+`;
 
 const SidebarBox = styled(OnlyAlignCenterFlex)`
-flex-direction : column;
-row-gap : 30px;
-
-`
+  flex-direction: column;
+  row-gap: 30px;
+`;
 
 const RelevantToTheUser = styled(AllCenterFlex)`
   padding: 15px 0 20px;
-  border-bottom : 1px solid rgba(128,128,128,0.3);
-
-`
+  border-bottom: 1px solid rgba(128, 128, 128, 0.3);
+`;
 
 const LogoBox = styled.div`
-pointer-events: none;
-`
+  pointer-events: none;
+`;
 
 const SideMenuContainer = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: column;
   row-gap: 30px;
-`
+`;
 
 const SideMenuFooter = styled.div`
-  border-top : 1px solid rgba(128,128,128,0.3);
-  font-size :12px;
+  border-top: 1px solid rgba(128, 128, 128, 0.3);
+  font-size: 12px;
   padding: 20px 0 0;
   line-height: 20px;
-`
+`;
