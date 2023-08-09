949c22c9e603d5791f14c138a3922f22d3fcaa3b
KimKW1007 <face676934@gmail.com>
2023-08-09 16:34:06 +0900

    feat : accountEditTabList - add bannerEditTabList

--- front/src/utils/accountEditTabList.ts
+++ front/src/utils/accountEditTabList.ts
@@ -1,4 +1,5 @@
 
 
-export const accountEditTabList = [ "개인정보변경", "비밀번호변경","회원탈퇴"]
+export const accountEditTabList = [ "개인정보변경", "비밀번호변경","회원탈퇴", "배너설정"]
+export const bannerEditTabList = [ "메인배너", "프로젝트베너"]
 export const accountEditMobTabList = [ "카테고리", "개인정보변경", "비밀번호변경","회원탈퇴"]
