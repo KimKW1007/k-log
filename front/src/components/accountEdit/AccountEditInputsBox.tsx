7fbed302b3332f9e2f809ee3553548e2b59c6766
KimKW1007 <face676934@gmail.com>
2023-08-09 16:13:35 +0900

    feat : AccountEditInputsBox - 배너 관련 추가 (EditProject / EditBanner )

--- front/src/components/accountEdit/AccountEditInputsBox.tsx
+++ front/src/components/accountEdit/AccountEditInputsBox.tsx
@@ -7,19 +7,29 @@ import EditSideBar from './EditSidbar/EditSideBarBox';
 import { useRecoilState } from 'recoil';
 import { userInfomation } from '@atoms/atoms';
 import Withdraw from './Withdraw';
+import EditProject from './banner/EditProject';
+import EditBanner from './banner/EditBanner';
 
-const AccountEditInputsBox = ({currentTab } : {currentTab :string;}) => {
+const AccountEditInputsBox = ({currentTab, currentBannerTitle } : {[key:string] :string;}) => {
   const [currentUser, setCurrentUser] = useRecoilState(userInfomation);
 
   return (
     <InputsWrap>
-      <InputsBox> 
-        {currentTab === '카테고리' && <EditSideBar isAdmin={currentUser?.id === 1}></EditSideBar>}
+      <InputsBox isMainBanner={currentTab === '배너설정'}> 
+        {currentTab === '카테고리' && <EditSideBar isAdmin={currentUser?.id === 1}/>}
         {currentTab === '개인정보변경' && accountEditInputList.map(({ name, title }, idx) => (
-          <AccountEditBox title={title} key={idx + 'salt' + title} name={name} ></AccountEditBox>
+          <AccountEditBox title={title} key={idx + 'salt' + title} name={name} />
         ))}
-        {currentTab === '비밀번호변경' && <ChangePassword></ChangePassword>}
-        {currentTab === '회원탈퇴' && <Withdraw></Withdraw>}
+        {currentTab === '비밀번호변경' && <ChangePassword/>}
+        {currentTab === '회원탈퇴' && <Withdraw/>}
+        {currentTab === '배너설정' && <>
+          {currentBannerTitle === '메인배너'?
+          <EditBanner></EditBanner>
+        :
+        <EditProject></EditProject>
+        }
+        
+        </>}
       </InputsBox>
     </InputsWrap>
   );
@@ -31,10 +41,13 @@ const InputsWrap = styled.div`
   width: 100%;
 `;
 
-const InputsBox = styled.div`
+const InputsBox = styled.div<{isMainBanner ?: boolean;}>`
   position:relative;
   width: 100%;
   padding: 40px;
+  ${({isMainBanner}) => isMainBanner&&`
+    padding: 40px 0;
+  `}
   @media(max-width: 1080px){
     padding: 40px 0;
   }
