f3bbed73b2ddb6c52f42c5a84feef53991c18415
KimKW1007 <face676934@gmail.com>
2023-08-09 16:18:08 +0900

    feat : BannerItem - api 연결

--- front/src/components/banner/BannerItem.tsx
+++ front/src/components/banner/BannerItem.tsx
@@ -1,4 +1,4 @@
-import {banner1, banner2, banner3} from '@utils/bannerList';
+import {defaultBanner1, defaultBanner2, defaultBanner3} from '@utils/bannerList';
 import React, { useEffect, useState } from 'react';
 import styled, { keyframes, css } from 'styled-components';
 
@@ -7,43 +7,66 @@ interface BannerItemProps {
   currentRotate: number;
   idx: number;
   resetRotate: boolean;
+  data : {
+    id : number;
+    imageUrl : string;
+    listNumber : string;
+  }[];
 }
 
-const BaanerItem = ({ currentRotate, idx, resetRotate }: BannerItemProps) => {
-  return <Item currentRotate={currentRotate} resetRotate={resetRotate} idx={idx}></Item>;
+interface ItemProps {
+  currentRotate: number;
+  idx: number;
+  resetRotate: boolean;
+  banner1 : string
+  banner2 : string
+  banner3 : string
+}
+
+
+const BaanerItem = ({ currentRotate, idx, resetRotate, data }: BannerItemProps) => {
+  const banner1 = data?.[0]?.imageUrl || defaultBanner1;
+  const banner2 = data?.[1]?.imageUrl || defaultBanner2;
+  const banner3 = data?.[2]?.imageUrl || defaultBanner3;
+  return <Item currentRotate={currentRotate} resetRotate={resetRotate} idx={idx} 
+banner1={banner1}
+banner2={banner2}
+banner3={banner3}
+  
+  ></Item>;
 };
 
 export default BaanerItem;
 // ${({idx}) => idx * 32 }
 // ${({ currentRotate }) => currentRotate}
 
-const Item = styled.div<{ currentRotate: number; idx: number; resetRotate: boolean; }>`
+const Item = styled.div<ItemProps>`
   position: absolute;
   z-index: 50;
   width: 39.8px;
   height: 100%;
   top: -6.5px;
   left: ${({idx}) => (idx * 39)}px;
-  background: url(${banner1}) no-repeat;
+  background: url(${({banner1}) => banner1}) no-repeat;
   background-size: 977px auto;
   background-position: -${({idx}) => (idx * 39)}px  center;
   transform-style: preserve-3d;
-  transform: rotateY(0deg);
+  transform: rotateY(${({ currentRotate }) => currentRotate}deg);
   transition: transform 2s ${({ idx }) => idx * 0.15}s;
   ${({ resetRotate }) => resetRotate && `transition: none`};
   transform-origin: center center -8.666px;
   image-rendering: -webkit-optimize-contrast; /* 웹킷 브라우저용 */
   image-rendering: pixelated; /* 일반 브라우저용 */
   &:after {
-    background: url(${banner2})  no-repeat;
-    width: 100%;
+    background: url(${({banner2}) => banner2})  no-repeat;
+    width: 102%;
     left: 102%;
     transform: rotateY(120deg);
     transform-origin: left top;
   }
   &:before {
-    background: url(${banner3}) no-repeat;
-    width: 100%;
+    background: url(${({banner3}) => banner3}) no-repeat;
+    width: 102%;
     left: -102%;
     transform: rotateY(240deg);
     transform-origin: right top;
@@ -63,7 +86,7 @@ const Item = styled.div<{ currentRotate: number; idx: number; resetRotate: boole
     width: 4.2475vw;
     background-size: 104.2689vw auto;
     background-position: -${({idx}) => (idx * 4.1622)}vw  center;
-    left: ${({idx}) => (idx * 4.1622)}%;
+    left: ${({idx}) => (idx * 4.19)}%;
     &:before,
     &:after {
       background-size: 104.2689vw auto;
