6955faaa53cab9e74745a66c9e2ce17e8fee9af7
KimKW1007 <face676934@gmail.com>
2023-08-09 16:36:13 +0900

    feat(image-db) : BannerModule

--- k-log-image-db/src/app.module.ts
+++ k-log-image-db/src/app.module.ts
@@ -3,9 +3,10 @@ import { FileModule } from './file/file.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AppController } from './app.controller';
+import { BannerModule } from './banner/banner.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig),FileModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig),FileModule, BannerModule],
   controllers: [AppController],
   providers: [],
 })
