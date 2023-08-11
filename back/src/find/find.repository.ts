f86185331105d4878878f437cc300fc73402e1af
KimKW1007 <face676934@gmail.com>
2023-08-11 12:59:50 +0900

    chore : find.repo - sendMail => html 변경

--- back/src/find/find.repository.ts
+++ back/src/find/find.repository.ts
@@ -26,13 +26,24 @@ export class FindRepository extends Repository<FindId> {
       token: payload,
     });
     try{
-     /*  await this.mailerService
+      await this.mailerService
       .sendMail({
-        from: mailConfig.mailId,
+        from: `"K : log" <${mailConfig.mailId}>`,
         to: `${userEmail}`,
-        subject: 'K-log 인증번호',
-        html: `<b>인증번호 : ${payload}</b>`,
-      }) */
+        subject: 'K : log 인증 코드',
+        html: `
+        <div style="width:100%;  background-color:#e4e4e4; box-sizing: border-box; padding : 50px 0;">
+          <div style="width:600px; height: 420px; margin: 0 auto; box-sizing: border-box; padding : 50px; border-top: 8px solid #6F61C0; background-color:#23262d; color:#fff;">
+            <h2 style="margin : 0; padding-bottom: 25px; margin-bottom: 65px; font-size: 20px; border-bottom:2px solid rgba(128,128,128,0.3)">K : Log</h2>
+            <div style="text-align:center;">
+              <p>회원님의 인증 코드는 다음과 같습니다.</p>
+              <br />
+              <br />
+              <b style="font-weight: 800; font-size: 28px; letter-spacing: 1px;">${payload}</b>
+            </div>
+          </div>
+        </div>`,
+      })
       await this.save(certification);
       console.log('전송완료/ 인증번호', payload)
     }catch(e){
