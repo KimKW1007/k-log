001afa9a81202c05d7d90b25e6239fcfaa2425a7
KimKW1007 <face676934@gmail.com>
2023-08-11 13:07:45 +0900

    chore : bannerList - 배너이미지변경

--- front/src/utils/bannerList.ts
+++ front/src/utils/bannerList.ts
@@ -1,9 +1,9 @@
 import winterBg from '@assets/images/winterBg.jpg';
 import fallBg from '@assets/images/fallBg.jpg';
 import springBg from '@assets/images/springBg.jpg';
-import bg1 from '@assets/images/bg1.svg';
-import bg2 from '@assets/images/bg2.svg';
-import bg3 from '@assets/images/bg3.svg';
+import bg1 from '@assets/images/mainslide_01.jpg';
+import bg2 from '@assets/images/blank_space.jpg';
+import bg3 from '@assets/images/simple.jpg';
 import { ListTypes } from './mapList';
 
 //  배경은 3개만
