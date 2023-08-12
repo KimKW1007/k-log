dc4c595d7fdea2a3c37eaf0bbd8b816aad207ba7
KimKW1007 <face676934@gmail.com>
2023-08-12 11:06:05 +0900

    chore : find.repo - sendMail => html 내용변경

--- back/src/find/find.repository.ts
+++ back/src/find/find.repository.ts
@@ -35,7 +35,10 @@ export class FindRepository extends Repository<FindId> {
         <div style="width:100%;  background-color:#e4e4e4; box-sizing: border-box; padding : 50px 0;">
           <div style="width:600px; height: 420px; margin: 0 auto; box-sizing: border-box; padding : 50px; border-top: 8px solid #6F61C0; background-color:#23262d; color:#fff;">
             <h2 style="margin : 0; padding-bottom: 25px; margin-bottom: 65px; font-size: 20px; border-bottom:2px solid rgba(128,128,128,0.3)">K : Log</h2>
-            <div style="text-align:center;">
+            <div style="text-align:center; font-size: 16px;">
+              <p>K : Log 를 이용해 주셔서 감사합니다.</p>
+              <br />
+              <br />
               <p>회원님의 인증 코드는 다음과 같습니다.</p>
               <br />
               <br />
