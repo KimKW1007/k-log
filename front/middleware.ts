b45754a53ad08f8fc0b33bf1df2e94e2495ecc6c
KimKW1007 <face676934@gmail.com>
2023-08-13 16:50:16 +0900

    test : middleware 배포 시 오류 제거 test

--- front/middleware.ts
+++ front/middleware.ts
@@ -1,18 +1,18 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse, userAgent } from "next/server";
 
-export async function middleware( req: NextRequest ) {
-  let cookie = req.cookies.get('access_token')?.value
-  if(Boolean(cookie)){
-    if(req.url.includes('/login') || req.url.includes('/signup')){
+export async function middleware(req: NextRequest, res: NextResponse) {
+  let cookie = req.cookies.get('access_token')?.value;
+  if (Boolean(cookie)) {
+    if (req.url.includes('/login') || req.url.includes('/signup')) {
       return NextResponse.redirect(new URL("/", req.url));
     }
-
-  }else{
-    if(req.url.includes('/create') || req.url.includes('/edit') || req.url.includes('/accountEdit') ){
+  } else {
+    if (req.url.includes('/create') || req.url.includes('/edit') || req.url.includes('/accountEdit')) {
       return NextResponse.redirect(new URL("/", req.url));
     }
   }
 
+  return NextResponse.next();
 }
 
 export const config = {
