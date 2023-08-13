3f5c9c40d11a38dc200906fdffc9a5cf7c8779ea
KimKW1007 <face676934@gmail.com>
2023-08-13 10:58:17 +0900

    test(back) : typeorm.config

--- back/src/configs/typeorm.config.ts
+++ back/src/configs/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 
-export const typeORMConfig: DataSourceOptions = {
+export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: 5432,
@@ -10,9 +10,12 @@ export const typeORMConfig: DataSourceOptions = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: false,
+  synchronize: true,
   migrations: [__dirname + '/../**/migrations/*.ts'],
   migrationsTableName: 'migrations',
+  extra:{
+    timezone:'Asia/Seoul'
+  }
 };
 
-export default new DataSource(typeORMConfig)
+// export default new DataSource(typeORMConfig)
