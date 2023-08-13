95bbacf701a8b4eb1d71688f7375ad33896c7b2a
KimKW1007 <face676934@gmail.com>
2023-08-13 17:53:10 +0900

    test : middleware 배포테스트

--- front/middleware.ts
+++ front/middleware.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from 'next/server'
-export async function middleware(req: NextRequest) {
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+export const middleware =  (req: NextRequest) => {
   let cookie = req.cookies.get('access_token')?.value;
   if (Boolean(cookie)) {
     if (req.nextUrl.pathname.includes('/login') || req.nextUrl.pathname.includes('/signup')) {
-      return NextResponse.redirect(new URL("/", req.url));
+      return NextResponse.rewrite(new URL('/', req.url));
     }
   } else {
     if (req.nextUrl.pathname.includes('/create') || req.nextUrl.pathname.includes('/edit') || req.nextUrl.pathname.includes('/accountEdit')) {
-      return NextResponse.redirect(new URL("/", req.url));
+      return NextResponse.rewrite(new URL('/', req.url));
     }
   }
-}
+};
 
 export const config = {
-  matcher: ["/((?!api|_next/static|favicon.ico|static).*)"],
+  matcher: ['/((?!api|_next/static|favicon.ico|static).*)']
 };
