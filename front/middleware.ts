99b42ae5aeafabfb3a709a80d5f6c38b5d9c8abc
KimKW1007 <face676934@gmail.com>
2023-08-13 17:24:25 +0900

    test : middleware 배포테스트

--- front/middleware.ts
+++ front/middleware.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   let cookie = req.cookies.get('access_token')?.value;
   if (Boolean(cookie)) {
-    if (req.url.includes('/login') || req.url.includes('/signup')) {
+    if (req.nextUrl.pathname.includes('/login') || req.nextUrl.pathname.includes('/signup')) {
       return NextResponse.redirect(new URL("/", req.url));
     }
   } else {
-    if (req.url.includes('/create') || req.url.includes('/edit') || req.url.includes('/accountEdit')) {
+    if (req.nextUrl.pathname.includes('/create') || req.nextUrl.pathname.includes('/edit') || req.nextUrl.pathname.includes('/accountEdit')) {
       return NextResponse.redirect(new URL("/", req.url));
     }
   }
