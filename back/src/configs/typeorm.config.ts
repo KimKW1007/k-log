af402255f7733f90fc3b5480a57b05ac5f1827bd
KimKW1007 <face676934@gmail.com>
2023-08-13 09:18:44 +0900

    refactor(back) : package/typeorm.config - timezone 설정

--- back/src/configs/typeorm.config.ts
+++ back/src/configs/typeorm.config.ts
@@ -1,18 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import * as config from "config"
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-const dbConfig = config.get("db");
 
 export const typeORMConfig: DataSourceOptions = {
-  type: process.env.DB_TYPE || dbConfig.type,
-  host: process.env.DB_PORT || dbConfig.host,
-  port: process.env.DB_HOST || dbConfig.port,
-  username: process.env.DB_USERNAME || dbConfig.username,
-  password: process.env.DB_PASSWORD || dbConfig.password,
-  database: process.env.DB_DATABASE || dbConfig.database,
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  timezone: 'Asia/Seoul',
   synchronize: false,
   migrations: [__dirname + '/../**/migrations/*.ts'],
   migrationsTableName: 'migrations',
