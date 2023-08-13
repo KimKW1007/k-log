359ff0710cc0dbd22372547cfe00b70f1fe219d7
KimKW1007 <face676934@gmail.com>
2023-08-13 17:29:07 +0900

    test : middleware - 배포테스트

--- front/middleware.ts
+++ front/middleware.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-
+import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server'
 export async function middleware(req: NextRequest) {
   let cookie = req.cookies.get('access_token')?.value;
   if (Boolean(cookie)) {
