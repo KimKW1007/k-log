9af0c2b92178d2dc9ce7476f087e1e975a8d794f
KimKW1007 <face676934@gmail.com>
2023-08-14 11:16:33 +0900

    test(back) : auth.cont - singin cookie test

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -45,9 +45,11 @@ export class AuthController {
     res.setHeader('Authorization', 'Bearer ' + accessToken);
     res.cookie('access_token', accessToken, {
       httpOnly: true,
+      domain: 'localhost'
     });
     res.cookie('refresh_token', refreshToken, {
       httpOnly: true,
+      domain: 'localhost'
     });
     return res.json({
       message:'login_success',
