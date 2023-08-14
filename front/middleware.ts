ba1638cbd09e0756cc3ecfcfcc0483dd683f7f14
KimKW1007 <face676934@gmail.com>
2023-08-14 11:18:10 +0900

    chore : middleware - rewrite -> redirect

--- front/middleware.ts
+++ front/middleware.ts
@@ -4,11 +4,11 @@ export const middleware =  (req: NextRequest) => {
   let cookie = req.cookies.get('access_token')?.value;
   if (Boolean(cookie)) {
     if (req.nextUrl.pathname.includes('/login') || req.nextUrl.pathname.includes('/signup')) {
-      return NextResponse.rewrite(new URL('/', req.url));
+      return NextResponse.redirect(new URL('/', req.url));
     }
   } else {
     if (req.nextUrl.pathname.includes('/create') || req.nextUrl.pathname.includes('/edit') || req.nextUrl.pathname.includes('/accountEdit')) {
-      return NextResponse.rewrite(new URL('/', req.url));
+      return NextResponse.redirect(new URL('/', req.url));
     }
   }
 };
