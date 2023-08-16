b3ff7321c92c023b220aed0f24f050e26657ca67
KimKW1007 <face676934@gmail.com>
2023-08-16 16:07:45 +0900

    chore : middleware - config 변경

--- front/middleware.ts
+++ front/middleware.ts
@@ -2,17 +2,20 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 export const middleware =  (req: NextRequest) => {
   let cookie = req.cookies.get('access_token')?.value;
+
   if (Boolean(cookie)) {
-    if (req.nextUrl.pathname.includes('/login') || req.nextUrl.pathname.includes('/signup')) {
+    if (req.url.includes('/login') || req.url.includes('/signup')) {
       return NextResponse.redirect(new URL('/', req.url));
     }
   } else {
-    if (req.nextUrl.pathname.includes('/create') || req.nextUrl.pathname.includes('/edit') || req.nextUrl.pathname.includes('/accountEdit')) {
+    if (req.url.includes('/create') || req.url.includes('/edit') || req.url.includes('/accountEdit')) {
       return NextResponse.redirect(new URL('/', req.url));
     }
   }
+
 };
 
 export const config = {
-  matcher: ['/category/:path*, /accountEdit, /accountEdit/changeEmail, /login, /signup ']
+  matcher: ['/((?!api|_next/static|favicon.ico|static).*)']
+
 };
