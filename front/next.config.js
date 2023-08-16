873eefec5483b6b702d6c0b834ac9a099ff7bf3c
KimKW1007 <face676934@gmail.com>
2023-08-16 16:51:21 +0900

    chore : next.config - images add domains

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "*"],
+    domains: ["localhost", `sel3.cloudtype.app`, "*"],
     minimumCacheTTL: 60,
   },
   compiler: {
