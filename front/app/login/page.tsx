e8f0ad3ad06bc37cb0e26d6bdbbe9e46f2df99e8
KimKW1007 <face676934@gmail.com>
2023-08-16 12:39:51 +0900

    chore: loginpage - metadata desc 변경

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const metadata: Metadata = {
   title: '로그인 | K : Log',
-  description:"K:Log를 알게 되신것에 대해 되게 감사합니다. 로그인 하시고 K:Log를 즐겨주세요!"
+  description:"K:Log를 알게 되신것에 대해 되게 감사합니다. 로그인 하시고 K:Log를 둘러보세요!"
 }
 
 const LoginPage : NextPage = () => {
