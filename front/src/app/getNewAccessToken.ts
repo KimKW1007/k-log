00964a920c65422c80b06fd3c1db0db1672287df
KimKW1007 <face676934@gmail.com>
2023-08-16 16:08:53 +0900

    feat  : getNewAccessToken

---
+++ front/src/app/getNewAccessToken.ts
@@ -0,0 +1,74 @@
+'use client';
+import React, { useEffect } from 'react'
+import customApi, { baseApi } from '../utils/customApi';
+import actions from '@/app/actions';
+import { useRecoilState } from 'recoil';
+import { userInfomation } from '../atoms/atoms';
+
+const getNewAccessToken = () => {
+  const [currentUser, setCurrentUser] = useRecoilState(userInfomation);
+  const TOKEN_REFRESH_INTERVAL = 5 * 60 * 1000;
+  
+  const getNewAccessTokenByrefreshToken = async () => {
+    try {
+      const {refresh_token, hasAccessToken} = await actions();
+      // 현재 시간과 토큰의 만료 시간을 비교하여 갱신 여부 결정
+      const tokenExpiration = sessionStorage.getItem('access_token_expiration'); // 세션 스토리지에 저장된 토큰 만료 시간
+      const currentTime = new Date().getTime();
+      const expirationTime = parseInt(tokenExpiration!, 10);
+      if (expirationTime && expirationTime - currentTime < TOKEN_REFRESH_INTERVAL) {
+        // 만료 시간이 일정 시간(5분) 미만으로 남았을 때 토큰 갱신
+        const response = await baseApi().post('/auth/refresh', { refresh_token });
+        const newAccessToken = response.data.newAccessToken;
+        // 새로운 엑세스 토큰과 유효기간 세션 스토리지에 저장
+        sessionStorage.setItem('access_token', newAccessToken);
+        sessionStorage.setItem('access_token_expiration', `${new Date().getTime() + process.env.NEXT_PUBLIC_ACCESS_TOKEN_EXPIRATION!}`);
+      }
+    } catch (error) {
+      console.error('토큰 갱신 실패:', error);
+    }
+  };
+  const checkAccessToken = async () => {
+    const {refresh_token, hasAccessToken} = await actions();
+    if (Boolean(refresh_token) && !hasAccessToken) {
+      try {
+        // 리프레시 토큰을 사용하여 새로운 엑세스 토큰을 발급받는 API 호출
+        const response = await baseApi().post('/auth/refresh', { refresh_token });
+        const newAccessToken = response.data.newAccessToken;
+        sessionStorage.setItem('access_token', newAccessToken);
+        // 새로운 엑세스 토큰을 반환
+      } catch (error) {
+        console.error('새로운 엑세스 토큰 발급 실패:', error);
+      }
+    }
+  };
+  useEffect(() => {
+    let interval: string | number | NodeJS.Timeout | undefined;
+    (async()=>{
+      const {refresh_token, hasAccessToken} = await actions();
+      if (refresh_token) {
+        checkAccessToken();
+        getNewAccessTokenByrefreshToken();
+        interval = setInterval(getNewAccessTokenByrefreshToken, 60 * 1000);
+      }
+      return () => clearInterval(interval);
+    })()
+    return () => clearInterval(interval);
+  }, [currentUser]);
+
+  const { postApi: logoutApi } = customApi('/auth/cleanCookie');
+  useEffect(() => {
+    if (!sessionStorage.getItem('access_token')) {
+      const logoutAsync = async () => {
+        try {
+          await logoutApi({});
+        } catch (e) {
+          console.log('Error logging out:', e);
+        }
+      };
+      logoutAsync();
+    }
+  }, []);
+}
+
+export default getNewAccessToken
