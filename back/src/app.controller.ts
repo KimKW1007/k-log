0129abf6e89b0ed6506a39e7dab844055317a2f3
KimKW1007 <face676934@gmail.com>
2023-08-19 09:47:05 +0900

    test (back) : vercel 배포

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,10 +1,11 @@
 import { HttpStatus, Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
 
   @Get()
-  getMain(@Res() res) {
+  getMain(@Res() res ) {
     return res.status(HttpStatus.OK).json('hello server opened k-log');
   }
 }
