efff08197a91c34fd6bef05eb2951bf7999a51cb
KimKW1007 <face676934@gmail.com>
2023-08-22 20:56:56 +0900

    feat : Footer - notion 추가

--- front/src/components/footer/Footer.tsx
+++ front/src/components/footer/Footer.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 import logo_white from '@/src/assets/images/white.svg';
+import notion from '@/src/assets/images/notion.png';
+import Link from 'next/link';
+
 const Footer = () => {
   return (
     <FooterWrap>
@@ -15,13 +18,37 @@ const Footer = () => {
           COPYRIGHT &copy; 2023 Kim Kyenong Won.
           <br /> ALL RIGHTS RESERVED.
         </FooterCopyRight>
-        <EmptyBox></EmptyBox>
+        <SNSBox>
+          <Link href={'https://www.notion.so/5cc93c00074a43c2834d67c2276a2153?pvs=4'} title='노션'>
+            <span className='blind'>노션</span>
+          </Link>
+        </SNSBox>
       </FooterContainer>
     </FooterWrap>
   );
 };
 
 export default Footer;
+
+
+
+const SNSBox = styled.div`
+  width: 30px;
+  height:30px;
+  a{
+    display:block;
+    width:100%;
+    height:100%;
+    background : url(${notion.src}) no-repeat center center/100% 100%;
+    transition: opacity .2s;
+    &:hover{
+      opacity : .5;
+    }
+  }
+  @media(max-width: 470px){
+    display:none;
+  }
+`
 
 const EmptyBox = styled.div`
   @media (max-width: 800px) {
@@ -59,7 +86,6 @@ const FooterLogoBox = styled(OnlyAlignCenterFlex)`
     height: 40px;
   }
   @media (max-width: 700px) {
-    margin-right: 30px;
     img {
       width: 30px;
       height: 30px;
