363e2f50a1861d75b1dbc9ea9fb3be02fdb704e2
KimKW1007 <face676934@gmail.com>
2023-08-24 13:25:27 +0900

    refactor - createBlockquoteBox - 구조 변경

--- front/src/utils/createBlockquoteBox.ts
+++ front/src/utils/createBlockquoteBox.ts
@@ -7,18 +7,23 @@ const createBlockquoteBox = (ref : React.RefObject<HTMLDivElement>) => {
     parentDiv.classList.add('blockquote-box');
     const parentInnerDiv = document.createElement('div');
     parentInnerDiv.classList.add('blockquote-inner-box');
-    const childDiv = document.createElement('div');
-
-    // 첫 번째 자식은 childDiv에 추가하지 않고, 그 이후 요소들을 모두 childDiv에 추가
-    for (let i = 1; i < elements.length; i++) {
-      childDiv.appendChild(elements[i]);
-    }
-
-    // 원래 요소(elements[0]) 앞에 childDiv를 추가하여 첫 번째 자식 제외한 요소들을 묶어줍니다.
-    elements[0].parentNode.insertBefore(childDiv, elements[0].nextSibling);
+    let childDiv;
+    if(elements.length > 1){
+        childDiv = document.createElement('div');
+    
+        // 첫 번째 자식은 childDiv에 추가하지 않고, 그 이후 요소들을 모두 childDiv에 추가
+        for (let i = 1; i < elements.length; i++) {
+          childDiv.appendChild(elements[i]);
+        }
+    
+        // 원래 요소(elements[0]) 앞에 childDiv를 추가하여 첫 번째 자식 제외한 요소들을 묶어줍니다.
+        elements[0].parentNode.insertBefore(childDiv, elements[0].nextSibling);
+      }
     elements[0].parentNode.insertBefore(parentInnerDiv, elements[0]);
     parentInnerDiv.appendChild(elements[0]);
-    parentInnerDiv.appendChild(childDiv);
+    if(childDiv){
+      parentInnerDiv.appendChild(childDiv);
+    }
     if(parentInnerDiv){
       parentInnerDiv.parentNode?.insertBefore(parentDiv, parentInnerDiv);
       parentDiv.appendChild(parentInnerDiv);
