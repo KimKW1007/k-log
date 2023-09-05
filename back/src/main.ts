c86c2c0535223257566c5fb1e14e61a1af896b5c
KimKW1007 <face676934@gmail.com>
2023-09-05 11:58:43 +0900

    refactor(back) : main - request entity too large  / remove limit

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,13 +4,15 @@ import * as cookieParser from "cookie-parser"
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
-
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   config()
   const configService = app.get(ConfigService);
   const clientUrl = configService.get("CLIENT_URL");
+  app.use(express.json({limit: '50mb'}));
+  app.use(express.urlencoded({limit: '50mb'}));
   app.enableCors({
     origin:[`${clientUrl}`,'http://localhost:3000' ],
     credentials:true
