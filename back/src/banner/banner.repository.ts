a3103b2ecaa00d795ce1d65bfb9e848149684ac8
KimKW1007 <face676934@gmail.com>
2023-09-06 20:37:49 +0900

    refactor(back) : banner.repo - image-db 관련 코드 제거 및  imgur api 연동

--- back/src/banner/banner.repository.ts
+++ back/src/banner/banner.repository.ts
@@ -11,6 +11,7 @@ import { Banner } from './banner.entity';
 import { User } from 'src/auth/user.entity';
 import axios from 'axios';
 import { ConfigService } from '@nestjs/config';
+import uploads from 'src/utils/imageUploads';
 
 
 @Injectable()
@@ -20,35 +21,21 @@ export class BannerRepository extends Repository<Banner> {
     ) {
     super(Banner, dataSource.createEntityManager());
   }
-  private readonly CREATE_URL =  this.configService.get("IMAGE_SERVER_BANNER_UPDATE_URL") || 'http://localhost:8000/api/banner/updateImage';
-  
 
   async updateBanner(listNumber: string, file: Express.Multer.File, user: User){
     if(!user.isAdmin) throw new UnauthorizedException('관리자 권한이 없습니다.')
     const foundBanner = await this.findOneBy({listNumber})
-    console.log({file})
-
-    const formData = new FormData();
-    file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
-    const imageBuffer = Buffer.from(file.buffer);
-    const imageBlob = new Blob([imageBuffer], { type: file.mimetype });
-    formData.append('image', imageBlob, file.originalname);
-    formData.append('listNumber', listNumber);
+  
     try{
-      const response = await axios.patch(this.CREATE_URL, formData , {
-        headers:{
-          'Content-Type' : "multipart/form-data"
-        }
-      })
-      const IMG_URL = response.data.url;
+      const imageUrl = await uploads(file.buffer, file.mimetype, this.configService.get("IMGUR_ID"))
       if(!foundBanner){
         const createBanner = this.create({
-          imageUrl: IMG_URL,
+          imageUrl,
           listNumber
         })
         await this.save(createBanner);
       }else{
-        foundBanner.imageUrl = IMG_URL;
+        foundBanner.imageUrl = imageUrl;
         await this.save(foundBanner);
       }
       return { message: 'success' };
